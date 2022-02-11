60dd0cbfff02c50a7e7d3dc87c155e4a85de9c8e
Geoffrey Ragot <geoffrey.ragot@gmail.com>
2022-02-11 15:51:04 +0100

    Setup main.

--- cmd/root.go
+++ cmd/root.go
@@ -1,18 +1,49 @@
 package cmd
 
 import (
+	"errors"
 	"github.com/spf13/viper"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"net/http"
 	"os"
+	payment "payment/pkg"
 	"strings"
 
 	"github.com/spf13/cobra"
+)
+
+const (
+	mongodbUriFlag      = "mongodb-uri"
+	mongodbDatabaseFlag = "mongodb-database"
 )
 
 var rootCmd = &cobra.Command{
 	Use:   "payment",
 	Short: "Payment api",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		return nil
+
+		mongodbUri := viper.GetString(mongodbUriFlag)
+		if mongodbUri == "" {
+			return errors.New("missing mongodb uri")
+		}
+
+		mongodbDatabase := viper.GetString(mongodbDatabaseFlag)
+		if mongodbDatabase == "" {
+			return errors.New("missing mongodb database name")
+		}
+
+		client, err := mongo.NewClient(options.Client().ApplyURI(mongodbUri))
+		if err != nil {
+			return err
+		}
+
+		db := client.Database(mongodbDatabase)
+
+		s := payment.NewDefaultService(db)
+		mux := payment.NewMux(s)
+
+		return http.ListenAndServe(":8080", mux)
 	},
 }
 
@@ -25,6 +56,8 @@ func Execute() {
 
 func init() {
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.Flags().String(mongodbUriFlag, "mongodb://localhost:27017", "MongoDB address")
+	rootCmd.Flags().String(mongodbDatabaseFlag, "payments", "MongoDB database name")
 
 	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_", "-", "_"))
 	viper.AutomaticEnv()
