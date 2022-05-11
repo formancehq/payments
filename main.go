c7d0426044b0f85b35f30815c66b0ee617baaf5b
Geoffrey Ragot <geoffrey.ragot@gmail.com>
2022-05-11 14:46:24 +0200

    Fix client code.

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-//go:generate openapi-generator generate -i swagger.yml -g go -o ./pkg/paymentclient --additional-properties=packageName:ledgerclient --git-user-id=numary --git-repo-id=payments-cloud --additional-properties=isGoSubmodule=true --additional-properties=packageName=paymentclient -t ./gentpl
+//go:generate openapi-generator generate -i swagger.yml -g go -o ./pkg/paymentclient --additional-properties=packageName:ledgerclient --git-user-id=numary --git-repo-id=payments --additional-properties=isGoSubmodule=true --additional-properties=packageName=paymentclient -t ./gentpl
 package main
 
 import "github.com/numary/payments/cmd"
