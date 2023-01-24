eb043fe95dac21de8fbdd46f57b45d55373bfd41
Lawrence Zawila <113581282+darkmatterpool@users.noreply.github.com>
2023-01-24 19:52:07 +0200

    feat: metadata update route & object support
    Signed-off-by: Lawrence Zawila <113581282+darkmatterpool@users.noreply.github.com>

--- internal/app/models/metadata.go
+++ internal/app/models/metadata.go
@@ -16,7 +16,7 @@ type Metadata struct {
 	Key       string    `bun:",pk,nullzero"`
 	Value     string
 
-	Changelog []MetadataChangelog `bun:",pk,nullzero"`
+	Changelog []MetadataChangelog `bun:",nullzero"`
 	Payment   *Payment            `bun:"rel:has-one,join:payment_id=id"`
 }
 
