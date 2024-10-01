2c1cc2fd7096f170317c37bc5e6cfbba6ee2c089
Crimson Thompson <crimson@formance.com>
2024-10-01 20:13:50 +0200

    feat(payments): test that plugin activities handle error unwrapping correctly

---
+++ internal/connectors/engine/activities/plugin_create_webhooks_test.go
@@ -0,0 +1,86 @@
+package activities_test
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/formancehq/go-libs/errorsutils"
+	"github.com/formancehq/payments/internal/connectors/engine/activities"
+	"github.com/formancehq/payments/internal/connectors/engine/plugins"
+	"github.com/formancehq/payments/internal/connectors/httpwrapper"
+	"github.com/formancehq/payments/internal/events"
+	"github.com/formancehq/payments/internal/models"
+	"github.com/formancehq/payments/internal/storage"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"go.temporal.io/sdk/temporal"
+	gomock "go.uber.org/mock/gomock"
+)
+
+var _ = Describe("Plugin Create Webhooks", func() {
+	var (
+		act            activities.Activities
+		p              *plugins.MockPlugins
+		s              *storage.MockStorage
+		evts           *events.Events
+		sampleResponse models.CreateWebhooksResponse
+	)
+
+	BeforeEach(func() {
+		evts = &events.Events{}
+		sampleResponse = models.CreateWebhooksResponse{Others: make([]models.PSPOther, 0)}
+	})
+
+	Context("plugin create webhook", func() {
+		var (
+			plugin *models.MockPlugin
+			req    activities.CreateWebhooksRequest
+		)
+
+		BeforeEach(func() {
+			ctrl := gomock.NewController(GinkgoT())
+			p = plugins.NewMockPlugins(ctrl)
+			s = storage.NewMockStorage(ctrl)
+			plugin = models.NewMockPlugin(ctrl)
+			act = activities.New(s, evts, p)
+			req = activities.CreateWebhooksRequest{
+				ConnectorID: models.ConnectorID{
+					Provider: "some_provider",
+				},
+			}
+		})
+
+		It("calls underlying plugin", func(ctx SpecContext) {
+			p.EXPECT().Get(req.ConnectorID).Return(plugin, nil)
+			plugin.EXPECT().CreateWebhooks(ctx, req.Req).Return(sampleResponse, nil)
+			res, err := act.PluginCreateWebhooks(ctx, req)
+			Expect(err).To(BeNil())
+			Expect(res.Others).To(Equal(sampleResponse.Others))
+		})
+
+		It("returns a retryable temporal error", func(ctx SpecContext) {
+			p.EXPECT().Get(req.ConnectorID).Return(plugin, nil)
+			plugin.EXPECT().CreateWebhooks(ctx, req.Req).Return(sampleResponse, fmt.Errorf("some string"))
+			_, err := act.PluginCreateWebhooks(ctx, req)
+			Expect(err).ToNot(BeNil())
+			temporalErr, ok := err.(*temporal.ApplicationError)
+			Expect(ok).To(BeTrue())
+			Expect(temporalErr.NonRetryable()).To(BeFalse())
+			Expect(temporalErr.Type()).To(Equal(req.ConnectorID.Provider))
+		})
+
+		It("returns a non-retryable temporal error", func(ctx SpecContext) {
+			wrappedErr := fmt.Errorf("some string: %w", httpwrapper.ErrStatusCodeClientError)
+			newErr := errorsutils.NewErrorWithExitCode(wrappedErr, http.StatusTeapot)
+
+			p.EXPECT().Get(req.ConnectorID).Return(plugin, nil)
+			plugin.EXPECT().CreateWebhooks(ctx, req.Req).Return(sampleResponse, newErr)
+			_, err := act.PluginCreateWebhooks(ctx, req)
+			Expect(err).ToNot(BeNil())
+			temporalErr, ok := err.(*temporal.ApplicationError)
+			Expect(ok).To(BeTrue())
+			Expect(temporalErr.NonRetryable()).To(BeTrue())
+			Expect(temporalErr.Type()).To(Equal(req.ConnectorID.Provider))
+		})
+	})
+})
