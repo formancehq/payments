e386c1f51d9c0ea6963bd33c4bdb08dea99f8536
Crimson Thompson <crimson@formance.com>
2024-10-08 11:22:51 +0200

    feat(payments): use pointer receiver for wise plugin for consistency

--- internal/connectors/plugins/public/wise/accounts.go
+++ internal/connectors/plugins/public/wise/accounts.go
@@ -16,7 +16,7 @@ type accountsState struct {
 	LastAccountID uint64 `json:"lastAccountID"`
 }
 
-func (p Plugin) fetchNextAccounts(ctx context.Context, req models.FetchNextAccountsRequest) (models.FetchNextAccountsResponse, error) {
+func (p *Plugin) fetchNextAccounts(ctx context.Context, req models.FetchNextAccountsRequest) (models.FetchNextAccountsResponse, error) {
 	var oldState accountsState
 	if req.State != nil {
 		if err := json.Unmarshal(req.State, &oldState); err != nil {
