404723a5b0c75b97143f4947182189ec8d12bc7f
Crimson Thompson <crimson@formance.com>
2024-10-08 17:34:44 +0200

    feat(payments): add unit test for wise accounts and balances methods

---
+++ internal/connectors/plugins/public/wise/balances_test.go
@@ -0,0 +1,73 @@
+package wise
+
+import (
+	"encoding/json"
+	"fmt"
+	"math/big"
+
+	"github.com/formancehq/payments/internal/connectors/plugins/public/wise/client"
+	"github.com/formancehq/payments/internal/models"
+	"go.uber.org/mock/gomock"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Wise Plugin Balances", func() {
+	var (
+		plg *Plugin
+		m   *client.MockClient
+	)
+
+	BeforeEach(func() {
+		plg = &Plugin{}
+
+		ctrl := gomock.NewController(GinkgoT())
+		m = client.NewMockClient(ctrl)
+		plg.SetClient(m)
+	})
+
+	Context("fetch next balances", func() {
+		var (
+			balance           client.Balance
+			expectedProfileID uint64
+			profileVal        uint64
+		)
+
+		BeforeEach(func() {
+			expectedProfileID = 123454
+			profileVal = 999999
+			balance = client.Balance{
+				ID:     14556,
+				Type:   "type1",
+				Amount: client.BalanceAmount{Value: json.Number("44.99"), Currency: "USD"},
+			}
+		})
+
+		It("fetches balances from wise client", func(ctx SpecContext) {
+			req := models.FetchNextBalancesRequest{
+				State: json.RawMessage(`{}`),
+				FromPayload: json.RawMessage(fmt.Sprintf(
+					`{"Reference":"%d","Metadata":{"%s":"%d"}}`,
+					expectedProfileID,
+					MetadataProfileIDKey,
+					profileVal,
+				)),
+				PageSize: 10,
+			}
+			m.EXPECT().GetBalance(ctx, profileVal, expectedProfileID).Return(
+				&balance,
+				nil,
+			)
+
+			res, err := plg.FetchNextBalances(ctx, req)
+			Expect(err).To(BeNil())
+			Expect(res.HasMore).To(BeFalse())
+			Expect(res.Balances).To(HaveLen(1)) // always returns 1
+			Expect(res.Balances[0].AccountReference).To(Equal(fmt.Sprint(expectedProfileID)))
+			expectedBalance, err := balance.Amount.Value.Float64()
+			Expect(err).To(BeNil())
+			Expect(res.Balances[0].Amount).To(BeEquivalentTo(big.NewInt(int64(expectedBalance * 100))))
+		})
+	})
+})
