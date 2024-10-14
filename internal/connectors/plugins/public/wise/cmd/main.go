e2bba8ac0e119d28e88afb822a8a696325ee44c1
Crimson Thompson <crimson@formance.com>
2024-10-14 17:59:10 +0200

    feat(fx plugins): use fx in all plugins

--- internal/connectors/plugins/public/wise/cmd/main.go
+++ internal/connectors/plugins/public/wise/cmd/main.go
@@ -1,21 +1,13 @@
 package main
 
 import (
-	"github.com/formancehq/payments/internal/connectors/grpc"
+	"github.com/formancehq/go-libs/service"
 	"github.com/formancehq/payments/internal/connectors/plugins"
 	"github.com/formancehq/payments/internal/connectors/plugins/public/wise"
-	"github.com/hashicorp/go-plugin"
+	"github.com/formancehq/payments/internal/models"
 )
 
 func main() {
-	// TODO(polo): metrics
-	plugin.Serve(&plugin.ServeConfig{
-		HandshakeConfig: grpc.Handshake,
-		Plugins: map[string]plugin.Plugin{
-			"psp": &grpc.PSPGRPCPlugin{Impl: plugins.NewGRPCImplem(&wise.Plugin{})},
-		},
-
-		// A non-nil value here enables gRPC serving for this plugin...
-		GRPCServer: plugin.DefaultGRPCServer,
-	})
+	pluginFn := func() models.Plugin { return &wise.Plugin{} }
+	service.Execute(plugins.NewPlugin("wise", pluginFn))
 }
