3a88090b278bc48e46f46a929bb099ec238b65f3
Crimson Thompson <crimson@formance.com>
2024-10-29 18:09:45 +0100

    feat(testing): add unit tests for api v2 connector handlers

---
+++ internal/api/v2/handler_connectors_reset_test.go
@@ -0,0 +1,56 @@
+package v2
+
+import (
+	"errors"
+	"net/http"
+	"net/http/httptest"
+
+	"github.com/formancehq/payments/internal/api/backend"
+	"github.com/formancehq/payments/internal/models"
+	"github.com/google/uuid"
+	. "github.com/onsi/ginkgo/v2"
+	"go.uber.org/mock/gomock"
+)
+
+var _ = Describe("API v2 Connectors reset", func() {
+	var (
+		handlerFn http.HandlerFunc
+		connID    models.ConnectorID
+	)
+	BeforeEach(func() {
+		connID = models.ConnectorID{Reference: uuid.New(), Provider: "psp"}
+	})
+
+	Context("reset connectors", func() {
+		var (
+			w *httptest.ResponseRecorder
+			m *backend.MockBackend
+		)
+		BeforeEach(func() {
+			w = httptest.NewRecorder()
+			ctrl := gomock.NewController(GinkgoT())
+			m = backend.NewMockBackend(ctrl)
+			handlerFn = connectorsReset(m)
+		})
+
+		It("should return a bad request error when connector ID is invalid", func(ctx SpecContext) {
+			req := prepareQueryRequest("connectorID", "invalid")
+			handlerFn(w, req)
+
+			assertExpectedResponse(w.Result(), http.StatusBadRequest, ErrInvalidID)
+		})
+
+		It("should return an internal server error when backend returns error", func(ctx SpecContext) {
+			expectedErr := errors.New("connectors reset err")
+			m.EXPECT().ConnectorsReset(gomock.Any(), gomock.Any()).Return(expectedErr)
+			handlerFn(w, prepareQueryRequest("connectorID", connID.String()))
+			assertExpectedResponse(w.Result(), http.StatusInternalServerError, "INTERNAL")
+		})
+
+		It("should return status no content on success", func(ctx SpecContext) {
+			m.EXPECT().ConnectorsReset(gomock.Any(), connID).Return(nil)
+			handlerFn(w, prepareQueryRequest("connectorID", connID.String()))
+			assertExpectedResponse(w.Result(), http.StatusNoContent, "")
+		})
+	})
+})
