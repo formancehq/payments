2a164532b1fd141ab059bb18ca824de005ea9887
Crimson Thompson <crimson@formance.com>
2024-10-29 15:00:19 +0100

    feat(testing): add unit tests for api v3 account handlers

---
+++ internal/api/v3/handler_accounts_balances_test.go
@@ -0,0 +1,65 @@
+package v3
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+
+	"github.com/formancehq/go-libs/v2/bun/bunpaginate"
+	"github.com/formancehq/payments/internal/api/backend"
+	"github.com/formancehq/payments/internal/models"
+	"github.com/google/uuid"
+	. "github.com/onsi/ginkgo/v2"
+	"go.uber.org/mock/gomock"
+)
+
+var _ = Describe("API v3 Accounts Balances", func() {
+	var (
+		handlerFn http.HandlerFunc
+		accID     models.AccountID
+	)
+	BeforeEach(func() {
+		connID := models.ConnectorID{Reference: uuid.New(), Provider: "psp"}
+		accID = models.AccountID{Reference: uuid.New().String(), ConnectorID: connID}
+	})
+
+	Context("list balances", func() {
+		var (
+			w *httptest.ResponseRecorder
+			m *backend.MockBackend
+		)
+		BeforeEach(func() {
+			w = httptest.NewRecorder()
+			ctrl := gomock.NewController(GinkgoT())
+			m = backend.NewMockBackend(ctrl)
+			handlerFn = accountsBalances(m)
+		})
+
+		It("should return a validation request error when account ID is invalid", func(ctx SpecContext) {
+			req := prepareQueryRequest("accountID", "invalidvalue")
+			handlerFn(w, req)
+
+			assertExpectedResponse(w.Result(), http.StatusBadRequest, ErrValidation)
+		})
+
+		It("should return an internal server error when backend returns error", func(ctx SpecContext) {
+			req := prepareQueryRequest("accountID", accID.String())
+			m.EXPECT().BalancesList(gomock.Any(), gomock.Any()).Return(
+				&bunpaginate.Cursor[models.Balance]{}, fmt.Errorf("balances list error"),
+			)
+			handlerFn(w, req)
+
+			assertExpectedResponse(w.Result(), http.StatusInternalServerError, "INTERNAL")
+		})
+
+		It("should return a cursor object", func(ctx SpecContext) {
+			req := prepareQueryRequest("accountID", accID.String())
+			m.EXPECT().BalancesList(gomock.Any(), gomock.Any()).Return(
+				&bunpaginate.Cursor[models.Balance]{}, nil,
+			)
+			handlerFn(w, req)
+
+			assertExpectedResponse(w.Result(), http.StatusOK, "cursor")
+		})
+	})
+})
---
+++ internal/api/v3/handler_accounts_get_test.go
@@ -0,0 +1,64 @@
+package v3
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+
+	"github.com/formancehq/payments/internal/api/backend"
+	"github.com/formancehq/payments/internal/models"
+	"github.com/google/uuid"
+	. "github.com/onsi/ginkgo/v2"
+	"go.uber.org/mock/gomock"
+)
+
+var _ = Describe("API v3 Accounts", func() {
+	var (
+		handlerFn http.HandlerFunc
+		accID     models.AccountID
+	)
+	BeforeEach(func() {
+		connID := models.ConnectorID{Reference: uuid.New(), Provider: "psp"}
+		accID = models.AccountID{Reference: uuid.New().String(), ConnectorID: connID}
+	})
+
+	Context("get accounts", func() {
+		var (
+			w *httptest.ResponseRecorder
+			m *backend.MockBackend
+		)
+		BeforeEach(func() {
+			w = httptest.NewRecorder()
+			ctrl := gomock.NewController(GinkgoT())
+			m = backend.NewMockBackend(ctrl)
+			handlerFn = accountsGet(m)
+		})
+
+		It("should return an invalid ID error when account ID is invalid", func(ctx SpecContext) {
+			req := prepareQueryRequest("accountID", "invalidvalue")
+			handlerFn(w, req)
+
+			assertExpectedResponse(w.Result(), http.StatusBadRequest, ErrInvalidID)
+		})
+
+		It("should return an internal server error when backend returns error", func(ctx SpecContext) {
+			req := prepareQueryRequest("accountID", accID.String())
+			m.EXPECT().AccountsGet(gomock.Any(), accID).Return(
+				&models.Account{}, fmt.Errorf("accounts get error"),
+			)
+			handlerFn(w, req)
+
+			assertExpectedResponse(w.Result(), http.StatusInternalServerError, "INTERNAL")
+		})
+
+		It("should return data object", func(ctx SpecContext) {
+			req := prepareQueryRequest("accountID", accID.String())
+			m.EXPECT().AccountsGet(gomock.Any(), accID).Return(
+				&models.Account{}, nil,
+			)
+			handlerFn(w, req)
+
+			assertExpectedResponse(w.Result(), http.StatusOK, "data")
+		})
+	})
+})
