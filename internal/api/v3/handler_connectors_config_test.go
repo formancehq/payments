ae2ef7c74acc420a296948884c3d6f5d54435634
Crimson Thompson <crimson@formance.com>
2024-10-29 18:09:12 +0100

    feat(testing): add unit tests for api v3 connector handlers

---
+++ internal/api/v3/handler_connectors_config_test.go
@@ -0,0 +1,64 @@
+package v3
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+
+	"github.com/formancehq/payments/internal/api/backend"
+	"github.com/formancehq/payments/internal/models"
+	"github.com/google/uuid"
+	. "github.com/onsi/ginkgo/v2"
+	"go.uber.org/mock/gomock"
+)
+
+var _ = Describe("API v3 Connectors Config", func() {
+	var (
+		handlerFn http.HandlerFunc
+		connID    models.ConnectorID
+	)
+	BeforeEach(func() {
+		connID = models.ConnectorID{Reference: uuid.New(), Provider: "psp"}
+	})
+
+	Context("get connectors config", func() {
+		var (
+			w *httptest.ResponseRecorder
+			m *backend.MockBackend
+		)
+		BeforeEach(func() {
+			w = httptest.NewRecorder()
+			ctrl := gomock.NewController(GinkgoT())
+			m = backend.NewMockBackend(ctrl)
+			handlerFn = connectorsConfig(m)
+		})
+
+		It("should return an invalid ID error when connector ID is invalid", func(ctx SpecContext) {
+			req := prepareQueryRequest("connectorID", "invalidvalue")
+			handlerFn(w, req)
+
+			assertExpectedResponse(w.Result(), http.StatusBadRequest, ErrInvalidID)
+		})
+
+		It("should return an internal server error when backend returns error", func(ctx SpecContext) {
+			req := prepareQueryRequest("connectorID", connID.String())
+			m.EXPECT().ConnectorsConfig(gomock.Any(), connID).Return(
+				json.RawMessage("{}"), fmt.Errorf("connector configs get error"),
+			)
+			handlerFn(w, req)
+
+			assertExpectedResponse(w.Result(), http.StatusInternalServerError, "INTERNAL")
+		})
+
+		It("should return data object", func(ctx SpecContext) {
+			req := prepareQueryRequest("connectorID", connID.String())
+			m.EXPECT().ConnectorsConfig(gomock.Any(), connID).Return(
+				json.RawMessage("{}"), nil,
+			)
+			handlerFn(w, req)
+
+			assertExpectedResponse(w.Result(), http.StatusOK, "data")
+		})
+	})
+})
---
+++ internal/api/v3/handler_connectors_uninstall_test.go
@@ -0,0 +1,56 @@
+package v3
+
+import (
+	"errors"
+	"net/http"
+	"net/http/httptest"
+
+	"github.com/formancehq/payments/internal/api/backend"
+	"github.com/formancehq/payments/internal/models"
+	"github.com/google/uuid"
+	. "github.com/onsi/ginkgo/v2"
+	"go.uber.org/mock/gomock"
+)
+
+var _ = Describe("API v3 Connectors uninstall", func() {
+	var (
+		handlerFn http.HandlerFunc
+		connID    models.ConnectorID
+	)
+	BeforeEach(func() {
+		connID = models.ConnectorID{Reference: uuid.New(), Provider: "psp"}
+	})
+
+	Context("uninstall connectors", func() {
+		var (
+			w *httptest.ResponseRecorder
+			m *backend.MockBackend
+		)
+		BeforeEach(func() {
+			w = httptest.NewRecorder()
+			ctrl := gomock.NewController(GinkgoT())
+			m = backend.NewMockBackend(ctrl)
+			handlerFn = connectorsUninstall(m)
+		})
+
+		It("should return a bad request error when connector ID is invalid", func(ctx SpecContext) {
+			req := prepareQueryRequest("connectorID", "invalid")
+			handlerFn(w, req)
+
+			assertExpectedResponse(w.Result(), http.StatusBadRequest, ErrInvalidID)
+		})
+
+		It("should return an internal server error when backend returns error", func(ctx SpecContext) {
+			expectedErr := errors.New("connectors uninstall err")
+			m.EXPECT().ConnectorsUninstall(gomock.Any(), gomock.Any()).Return(expectedErr)
+			handlerFn(w, prepareQueryRequest("connectorID", connID.String()))
+			assertExpectedResponse(w.Result(), http.StatusInternalServerError, "INTERNAL")
+		})
+
+		It("should return status accepted on success", func(ctx SpecContext) {
+			m.EXPECT().ConnectorsUninstall(gomock.Any(), connID).Return(nil)
+			handlerFn(w, prepareQueryRequest("connectorID", connID.String()))
+			assertExpectedResponse(w.Result(), http.StatusAccepted, connID.String())
+		})
+	})
+})
