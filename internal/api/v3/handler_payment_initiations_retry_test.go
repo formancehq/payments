8b52fd55a24aabae795ef53273ffddf561d7ec7a
Crimson Thompson <crimson@formance.com>
2024-10-30 10:29:27 +0100

    feat(testing): add unit tests for api v3 payment initiation handlers

---
+++ internal/api/v3/handler_payment_initiations_retry_test.go
@@ -0,0 +1,63 @@
+package v3
+
+import (
+	"errors"
+	"net/http"
+	"net/http/httptest"
+
+	"github.com/formancehq/payments/internal/api/backend"
+	"github.com/formancehq/payments/internal/models"
+	"github.com/google/uuid"
+	. "github.com/onsi/ginkgo/v2"
+	"go.uber.org/mock/gomock"
+)
+
+var _ = Describe("API v3 Payment Initiation Retry", func() {
+	var (
+		handlerFn http.HandlerFunc
+		paymentID models.PaymentInitiationID
+	)
+	BeforeEach(func() {
+		connID := models.ConnectorID{Reference: uuid.New(), Provider: "psp"}
+		paymentID = models.PaymentInitiationID{Reference: "ref", ConnectorID: connID}
+	})
+
+	Context("retry payment initiation", func() {
+		var (
+			w *httptest.ResponseRecorder
+			m *backend.MockBackend
+		)
+		BeforeEach(func() {
+			w = httptest.NewRecorder()
+			ctrl := gomock.NewController(GinkgoT())
+			m = backend.NewMockBackend(ctrl)
+			handlerFn = paymentInitiationsRetry(m)
+		})
+
+		It("should return a bad request error when paymentInitiationID is invalid", func(ctx SpecContext) {
+			req := prepareQueryRequest("paymentInitiationID", "invalid")
+			handlerFn(w, req)
+
+			assertExpectedResponse(w.Result(), http.StatusBadRequest, ErrInvalidID)
+		})
+
+		It("should return an internal server error when backend returns error", func(ctx SpecContext) {
+			expectedErr := errors.New("payment initiation retry err")
+			m.EXPECT().PaymentInitiationsRetry(gomock.Any(), gomock.Any(), false).Return(
+				models.Task{},
+				expectedErr,
+			)
+			handlerFn(w, prepareQueryRequest("paymentInitiationID", paymentID.String()))
+			assertExpectedResponse(w.Result(), http.StatusInternalServerError, "INTERNAL")
+		})
+
+		It("should return status accepted on success", func(ctx SpecContext) {
+			m.EXPECT().PaymentInitiationsRetry(gomock.Any(), paymentID, false).Return(
+				models.Task{},
+				nil,
+			)
+			handlerFn(w, prepareQueryRequest("paymentInitiationID", paymentID.String()))
+			assertExpectedResponse(w.Result(), http.StatusAccepted, "data")
+		})
+	})
+})
