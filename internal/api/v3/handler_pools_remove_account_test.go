56a3a5e4554a8c16adfbbc35231041bc2ebfe591
Crimson Thompson <crimson@formance.com>
2024-10-30 15:17:21 +0100

    feat(testing): add unit tests for api v3 pools handlers

---
+++ internal/api/v3/handler_pools_remove_account_test.go
@@ -0,0 +1,66 @@
+package v3
+
+import (
+	"errors"
+	"net/http"
+	"net/http/httptest"
+
+	"github.com/formancehq/payments/internal/api/backend"
+	"github.com/formancehq/payments/internal/models"
+	"github.com/google/uuid"
+	. "github.com/onsi/ginkgo/v2"
+	"go.uber.org/mock/gomock"
+)
+
+var _ = Describe("API v3 pools remove account", func() {
+	var (
+		handlerFn http.HandlerFunc
+		accID     models.AccountID
+		poolID    uuid.UUID
+	)
+	BeforeEach(func() {
+		connID := models.ConnectorID{Reference: uuid.New(), Provider: "psp"}
+		accID = models.AccountID{Reference: uuid.New().String(), ConnectorID: connID}
+		poolID = uuid.New()
+	})
+
+	Context("pool remove account", func() {
+		var (
+			w *httptest.ResponseRecorder
+			m *backend.MockBackend
+		)
+		BeforeEach(func() {
+			w = httptest.NewRecorder()
+			ctrl := gomock.NewController(GinkgoT())
+			m = backend.NewMockBackend(ctrl)
+			handlerFn = poolsRemoveAccount(m)
+		})
+
+		It("should return a bad request error when poolID is invalid", func(ctx SpecContext) {
+			req := prepareQueryRequest("poolID", "invalid")
+			handlerFn(w, req)
+
+			assertExpectedResponse(w.Result(), http.StatusBadRequest, ErrInvalidID)
+		})
+
+		It("should return a bad request error when accountID is invalid", func(ctx SpecContext) {
+			req := prepareQueryRequest("poolID", poolID.String(), "accountID", "invalid")
+			handlerFn(w, req)
+
+			assertExpectedResponse(w.Result(), http.StatusBadRequest, ErrInvalidID)
+		})
+
+		It("should return an internal server error when backend returns error", func(ctx SpecContext) {
+			expectedErr := errors.New("pool remove account err")
+			m.EXPECT().PoolsRemoveAccount(gomock.Any(), gomock.Any(), gomock.Any()).Return(expectedErr)
+			handlerFn(w, prepareQueryRequest("poolID", poolID.String(), "accountID", accID.String()))
+			assertExpectedResponse(w.Result(), http.StatusInternalServerError, "INTERNAL")
+		})
+
+		It("should return status no content on success", func(ctx SpecContext) {
+			m.EXPECT().PoolsRemoveAccount(gomock.Any(), poolID, accID).Return(nil)
+			handlerFn(w, prepareQueryRequest("poolID", poolID.String(), "accountID", accID.String()))
+			assertExpectedResponse(w.Result(), http.StatusNoContent, "")
+		})
+	})
+})
