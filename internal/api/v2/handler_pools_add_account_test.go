b912d5bcbe5f054362a3effbb84ca3ace7ac701e
Crimson Thompson <crimson@formance.com>
2024-10-30 15:17:58 +0100

    feat(testing): add unit tests for api v2 pools handlers

---
+++ internal/api/v2/handler_pools_add_account_test.go
@@ -0,0 +1,72 @@
+package v2
+
+import (
+	"errors"
+	"net/http"
+	"net/http/httptest"
+
+	"github.com/formancehq/payments/internal/api/backend"
+	"github.com/formancehq/payments/internal/models"
+	"github.com/google/uuid"
+	. "github.com/onsi/ginkgo/v2"
+	"go.uber.org/mock/gomock"
+)
+
+var _ = Describe("API v2 pools add account", func() {
+	var (
+		handlerFn http.HandlerFunc
+		accID     models.AccountID
+		poolID    uuid.UUID
+		paar      poolsAddAccountRequest
+	)
+	BeforeEach(func() {
+		connID := models.ConnectorID{Reference: uuid.New(), Provider: "psp"}
+		accID = models.AccountID{Reference: uuid.New().String(), ConnectorID: connID}
+		poolID = uuid.New()
+	})
+
+	Context("pool add account", func() {
+		var (
+			w *httptest.ResponseRecorder
+			m *backend.MockBackend
+		)
+		BeforeEach(func() {
+			w = httptest.NewRecorder()
+			ctrl := gomock.NewController(GinkgoT())
+			m = backend.NewMockBackend(ctrl)
+			handlerFn = poolsAddAccount(m)
+		})
+
+		It("should return a bad request error when body is missing", func(ctx SpecContext) {
+			handlerFn(w, prepareJSONRequestWithQuery(http.MethodPost, "poolID", poolID.String(), nil))
+
+			assertExpectedResponse(w.Result(), http.StatusBadRequest, ErrValidation)
+		})
+
+		It("should return a bad request error when poolID is invalid", func(ctx SpecContext) {
+			req := prepareQueryRequest("poolID", "invalid")
+			handlerFn(w, req)
+
+			assertExpectedResponse(w.Result(), http.StatusBadRequest, ErrInvalidID)
+		})
+
+		It("should return an internal server error when backend returns error", func(ctx SpecContext) {
+			expectedErr := errors.New("pool add account err")
+			m.EXPECT().PoolsAddAccount(gomock.Any(), gomock.Any(), gomock.Any()).Return(expectedErr)
+			paar = poolsAddAccountRequest{
+				AccountID: accID.String(),
+			}
+			handlerFn(w, prepareJSONRequestWithQuery(http.MethodPost, "poolID", poolID.String(), &paar))
+			assertExpectedResponse(w.Result(), http.StatusInternalServerError, "INTERNAL")
+		})
+
+		It("should return status no content on success", func(ctx SpecContext) {
+			m.EXPECT().PoolsAddAccount(gomock.Any(), poolID, accID).Return(nil)
+			paar = poolsAddAccountRequest{
+				AccountID: accID.String(),
+			}
+			handlerFn(w, prepareJSONRequestWithQuery(http.MethodPost, "poolID", poolID.String(), &paar))
+			assertExpectedResponse(w.Result(), http.StatusNoContent, "")
+		})
+	})
+})
