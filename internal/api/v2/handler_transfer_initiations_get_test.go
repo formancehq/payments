95cf3a387f1b3e102fe04d0a379c7a4ffe92d983
Crimson Thompson <crimson@formance.com>
2024-10-30 16:33:32 +0100

    feat(testing): add unit tests for api v2 transfer intiations handlers

---
+++ internal/api/v2/handler_transfer_initiations_get_test.go
@@ -0,0 +1,103 @@
+package v2
+
+import (
+	"errors"
+	"net/http"
+	"net/http/httptest"
+
+	"github.com/formancehq/payments/internal/api/backend"
+	"github.com/formancehq/payments/internal/models"
+	"github.com/google/uuid"
+	. "github.com/onsi/ginkgo/v2"
+	"go.uber.org/mock/gomock"
+)
+
+var _ = Describe("API v2 Transfer Initiation Get", func() {
+	var (
+		handlerFn http.HandlerFunc
+		paymentID models.PaymentInitiationID
+	)
+	BeforeEach(func() {
+		connID := models.ConnectorID{Reference: uuid.New(), Provider: "psp"}
+		paymentID = models.PaymentInitiationID{Reference: "ref", ConnectorID: connID}
+	})
+
+	Context("get payment initiation", func() {
+		var (
+			w *httptest.ResponseRecorder
+			m *backend.MockBackend
+		)
+		BeforeEach(func() {
+			w = httptest.NewRecorder()
+			ctrl := gomock.NewController(GinkgoT())
+			m = backend.NewMockBackend(ctrl)
+			handlerFn = transferInitiationsGet(m)
+		})
+
+		It("should return a bad request error when transferInitiationID is invalid", func(ctx SpecContext) {
+			req := prepareQueryRequest("transferInitiationID", "invalid")
+			handlerFn(w, req)
+
+			assertExpectedResponse(w.Result(), http.StatusBadRequest, ErrInvalidID)
+		})
+
+		It("should return an internal server error when backend returns error", func(ctx SpecContext) {
+			expectedErr := errors.New("payment initiation get err")
+			m.EXPECT().PaymentInitiationsGet(gomock.Any(), gomock.Any()).Return(
+				&models.PaymentInitiation{},
+				expectedErr,
+			)
+			handlerFn(w, prepareQueryRequest("transferInitiationID", paymentID.String()))
+			assertExpectedResponse(w.Result(), http.StatusInternalServerError, "INTERNAL")
+		})
+
+		It("should return an internal server error when backend returns error fetching payments", func(ctx SpecContext) {
+			expectedErr := errors.New("payment initiation get payments err")
+			m.EXPECT().PaymentInitiationsGet(gomock.Any(), gomock.Any()).Return(
+				&models.PaymentInitiation{},
+				nil,
+			)
+			m.EXPECT().PaymentInitiationRelatedPaymentListAll(gomock.Any(), gomock.Any()).Return(
+				[]models.Payment{},
+				expectedErr,
+			)
+			handlerFn(w, prepareQueryRequest("transferInitiationID", paymentID.String()))
+			assertExpectedResponse(w.Result(), http.StatusInternalServerError, "INTERNAL")
+		})
+
+		It("should return an internal server error when backend returns error fetching payment adjustments", func(ctx SpecContext) {
+			expectedErr := errors.New("payment initiation get adjustments err")
+			m.EXPECT().PaymentInitiationsGet(gomock.Any(), gomock.Any()).Return(
+				&models.PaymentInitiation{},
+				nil,
+			)
+			m.EXPECT().PaymentInitiationRelatedPaymentListAll(gomock.Any(), gomock.Any()).Return(
+				[]models.Payment{},
+				nil,
+			)
+			m.EXPECT().PaymentInitiationAdjustmentsListAll(gomock.Any(), paymentID).Return(
+				[]models.PaymentInitiationAdjustment{},
+				expectedErr,
+			)
+			handlerFn(w, prepareQueryRequest("transferInitiationID", paymentID.String()))
+			assertExpectedResponse(w.Result(), http.StatusInternalServerError, "INTERNAL")
+		})
+
+		It("should return status ok on success", func(ctx SpecContext) {
+			m.EXPECT().PaymentInitiationsGet(gomock.Any(), paymentID).Return(
+				&models.PaymentInitiation{},
+				nil,
+			)
+			m.EXPECT().PaymentInitiationRelatedPaymentListAll(gomock.Any(), gomock.Any()).Return(
+				[]models.Payment{},
+				nil,
+			)
+			m.EXPECT().PaymentInitiationAdjustmentsListAll(gomock.Any(), paymentID).Return(
+				[]models.PaymentInitiationAdjustment{},
+				nil,
+			)
+			handlerFn(w, prepareQueryRequest("transferInitiationID", paymentID.String()))
+			assertExpectedResponse(w.Result(), http.StatusOK, "data")
+		})
+	})
+})
