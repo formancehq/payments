b3ed43951f86ccc1acb7280c0ff869177cc65064
Paul Nicolas <paul.nicolas.go@gmail.com>
2024-10-30 11:39:14 +0100

    feat(bankingcircle): fix loop and add tests

---
+++ internal/connectors/plugins/public/bankingcircle/accounts_test.go
@@ -0,0 +1,170 @@
+package bankingcircle
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/formancehq/payments/internal/connectors/plugins/public/bankingcircle/client"
+	"github.com/formancehq/payments/internal/models"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"go.uber.org/mock/gomock"
+)
+
+var _ = Describe("BankingCircle Plugin Accounts", func() {
+	var (
+		plg *Plugin
+	)
+
+	BeforeEach(func() {
+		plg = &Plugin{}
+	})
+
+	Context("fetching next accounts", func() {
+		var (
+			m              *client.MockClient
+			sampleAccounts []client.Account
+			now            time.Time
+		)
+
+		BeforeEach(func() {
+			ctrl := gomock.NewController(GinkgoT())
+			m = client.NewMockClient(ctrl)
+			plg.client = m
+			now = time.Now().UTC()
+
+			sampleAccounts = make([]client.Account, 0)
+			for i := 0; i < 50; i++ {
+				sampleAccounts = append(sampleAccounts, client.Account{
+					AccountID:          fmt.Sprint(i),
+					AccountDescription: fmt.Sprintf("Account %d", i),
+					Currency:           "EUR",
+					OpeningDate:        now.Add(-time.Duration(50-i) * time.Minute).UTC().Format("2006-01-02T15:04:05.999999999+00:00"),
+				})
+			}
+		})
+
+		It("should return an error - get accounts error", func(ctx SpecContext) {
+			req := models.FetchNextAccountsRequest{
+				State:    []byte(`{}`),
+				PageSize: 60,
+			}
+
+			m.EXPECT().GetAccounts(ctx, 1, 60, time.Time{}).Return(
+				[]client.Account{},
+				errors.New("test error"),
+			)
+
+			resp, err := plg.FetchNextAccounts(ctx, req)
+			Expect(err).ToNot(BeNil())
+			Expect(err).To(MatchError("test error"))
+			Expect(resp).To(Equal(models.FetchNextAccountsResponse{}))
+		})
+
+		It("should fetch next accounts - no state no results", func(ctx SpecContext) {
+			req := models.FetchNextAccountsRequest{
+				State:    []byte(`{}`),
+				PageSize: 60,
+			}
+
+			m.EXPECT().GetAccounts(ctx, 1, 60, time.Time{}).Return(
+				[]client.Account{},
+				nil,
+			)
+
+			resp, err := plg.FetchNextAccounts(ctx, req)
+			Expect(err).To(BeNil())
+			Expect(resp.Accounts).To(HaveLen(0))
+			Expect(resp.HasMore).To(BeFalse())
+			Expect(resp.NewState).ToNot(BeNil())
+
+			var state accountsState
+			err = json.Unmarshal(resp.NewState, &state)
+			Expect(err).To(BeNil())
+			// We fetched everything, state should be resetted
+			Expect(state.LastAccountID).To(BeEmpty())
+			Expect(state.FromOpeningDate.IsZero()).To(BeTrue())
+		})
+
+		It("should fetch next accounts - no state pageSize > total accounts", func(ctx SpecContext) {
+			req := models.FetchNextAccountsRequest{
+				State:    []byte(`{}`),
+				PageSize: 60,
+			}
+
+			m.EXPECT().GetAccounts(ctx, 1, 60, time.Time{}).Return(
+				sampleAccounts,
+				nil,
+			)
+
+			resp, err := plg.FetchNextAccounts(ctx, req)
+			Expect(err).To(BeNil())
+			Expect(resp.Accounts).To(HaveLen(50))
+			Expect(resp.HasMore).To(BeFalse())
+			Expect(resp.NewState).ToNot(BeNil())
+
+			var state accountsState
+			err = json.Unmarshal(resp.NewState, &state)
+			Expect(err).To(BeNil())
+			// We fetched everything, state should be resetted
+			Expect(state.LastAccountID).To(Equal("49"))
+			Expect(state.FromOpeningDate).To(Equal(now.Add(-time.Duration(1) * time.Minute).UTC()))
+		})
+
+		It("should fetch next accounts - no state pageSize < total accounts", func(ctx SpecContext) {
+			req := models.FetchNextAccountsRequest{
+				State:    []byte(`{}`),
+				PageSize: 40,
+			}
+
+			m.EXPECT().GetAccounts(ctx, 1, 40, time.Time{}).Return(
+				sampleAccounts[:40],
+				nil,
+			)
+
+			resp, err := plg.FetchNextAccounts(ctx, req)
+			Expect(err).To(BeNil())
+			Expect(resp.Accounts).To(HaveLen(40))
+			Expect(resp.HasMore).To(BeTrue())
+			Expect(resp.NewState).ToNot(BeNil())
+
+			var state accountsState
+			err = json.Unmarshal(resp.NewState, &state)
+			Expect(err).To(BeNil())
+			Expect(state.LastAccountID).To(Equal("39"))
+			Expect(state.FromOpeningDate).To(Equal(now.Add(-time.Duration(11) * time.Minute).UTC()))
+		})
+
+		It("should fetch next accounts - with state pageSize < total accounts", func(ctx SpecContext) {
+			req := models.FetchNextAccountsRequest{
+				State:    []byte(fmt.Sprintf(`{"lastAccountID": "%d", "fromOpeningDate": "%s"}`, 38, now.Add(-time.Duration(12)*time.Minute).UTC().Format(time.RFC3339Nano))),
+				PageSize: 40,
+			}
+
+			m.EXPECT().GetAccounts(ctx, 1, 40, now.Add(-time.Duration(12)*time.Minute).UTC()).Return(
+				sampleAccounts[:40],
+				nil,
+			)
+
+			m.EXPECT().GetAccounts(ctx, 2, 40, now.Add(-time.Duration(12)*time.Minute).UTC()).Return(
+				sampleAccounts[40:],
+				nil,
+			)
+
+			resp, err := plg.FetchNextAccounts(ctx, req)
+			Expect(err).To(BeNil())
+			Expect(resp.Accounts).To(HaveLen(11))
+			Expect(resp.HasMore).To(BeFalse())
+			Expect(resp.NewState).ToNot(BeNil())
+
+			var state accountsState
+			err = json.Unmarshal(resp.NewState, &state)
+			Expect(err).To(BeNil())
+			// We fetched everything, state should be resetted
+			Expect(state.LastAccountID).To(Equal("49"))
+			Expect(state.FromOpeningDate).To(Equal(now.Add(-time.Duration(1) * time.Minute).UTC()))
+		})
+	})
+})
