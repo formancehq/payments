347999e345cb8b06b8e8e06dfcf0f96ba41c0d45
Crimson Thompson <crimson@formance.com>
2024-11-04 13:54:03 +0100

    feat(traces): add StartSpan convenience function

--- internal/otel/otel.go
+++ internal/otel/otel.go
@@ -1,9 +1,11 @@
 package otel
 
 import (
+	"context"
 	"sync"
 
 	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/codes"
 	"go.opentelemetry.io/otel/trace"
 )
@@ -24,4 +26,23 @@ func Tracer() trace.Tracer {
 func RecordError(span trace.Span, err error) {
 	span.RecordError(err)
 	span.SetStatus(codes.Error, err.Error())
+}
+
+func StartSpan(
+	ctx context.Context,
+	spanName string,
+	attributes ...attribute.KeyValue,
+) (context.Context, trace.Span) {
+	parentSpan := trace.SpanFromContext(ctx)
+	return Tracer().Start(
+		ctx,
+		spanName,
+		trace.WithNewRoot(),
+		trace.WithLinks(trace.Link{
+			SpanContext: parentSpan.SpanContext(),
+		}),
+		trace.WithAttributes(
+			attributes...,
+		),
+	)
 }
