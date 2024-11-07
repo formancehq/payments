f3c9b9a56e26e8c5945159f785ce4196fc7f6996
Paul Nicolas <paul.nicolas.go@gmail.com>
2024-11-07 12:10:02 +0100

    add tests

---
+++ internal/connectors/plugins/public/atlar/payments_test.go
@@ -0,0 +1,289 @@
+package atlar
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/formancehq/go-libs/v2/pointer"
+	"github.com/formancehq/payments/internal/connectors/plugins/public/atlar/client"
+	"github.com/formancehq/payments/internal/models"
+	"github.com/get-momo/atlar-v1-go-client/client/accounts"
+	"github.com/get-momo/atlar-v1-go-client/client/third_parties"
+	"github.com/get-momo/atlar-v1-go-client/client/transactions"
+	atlar_models "github.com/get-momo/atlar-v1-go-client/models"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"go.uber.org/mock/gomock"
+)
+
+var _ = Describe("Atlar Plugin Payments", func() {
+	var (
+		plg *Plugin
+	)
+
+	BeforeEach(func() {
+		plg = &Plugin{}
+	})
+
+	Context("fetching next payments", func() {
+		var (
+			m              *client.MockClient
+			samplePayments []*atlar_models.Transaction
+			sampleAccount  atlar_models.Account
+			now            time.Time
+		)
+
+		BeforeEach(func() {
+			ctrl := gomock.NewController(GinkgoT())
+			m = client.NewMockClient(ctrl)
+			plg.client = m
+			now = time.Now().UTC()
+
+			sampleAccount = atlar_models.Account{
+				Bank: &atlar_models.BankSlim{
+					Bic: "test",
+				},
+				Bic:          "test",
+				ThirdPartyID: "test",
+			}
+
+			samplePayments = make([]*atlar_models.Transaction, 0)
+			for i := 0; i < 50; i++ {
+				samplePayments = append(samplePayments, &atlar_models.Transaction{
+					ID: fmt.Sprintf("%d", i),
+					Amount: &atlar_models.Amount{
+						Currency:    pointer.For("EUR"),
+						StringValue: pointer.For("100"),
+						Value:       pointer.For(int64(100)),
+					},
+					Account: &atlar_models.AccountTrx{
+						ID: pointer.For("test"),
+					},
+					Characteristics: &atlar_models.TransactionCharacteristics{
+						BankTransactionCode: &atlar_models.BankTransactionCode{
+							Description: "test",
+							Domain:      "test",
+							Family:      "test",
+							Subfamily:   "test",
+						},
+					},
+					Created:     now.Add(-time.Duration(50-i) * time.Minute).UTC().Format(time.RFC3339Nano),
+					Description: "test",
+					Reconciliation: &atlar_models.ReconciliationDetails{
+						BookedTransactionID:   "test",
+						ExpectedTransactionID: "test",
+						Status:                "test",
+						TransactableID:        "test",
+						TransactableType:      "test",
+					},
+					RemittanceInformation: &atlar_models.RemittanceInformation{
+						Type:  pointer.For("test"),
+						Value: pointer.For("test"),
+					},
+				})
+			}
+		})
+
+		It("should return an error - get payments error", func(ctx SpecContext) {
+			req := models.FetchNextPaymentsRequest{
+				PageSize: 60,
+			}
+
+			m.EXPECT().GetV1Transactions(gomock.Any(), "", int64(60)).Return(
+				&transactions.GetV1TransactionsOK{
+					Payload: &transactions.GetV1TransactionsOKBody{
+						QueryResponse: atlar_models.QueryResponse{
+							NextToken: "",
+						},
+						Items: []*atlar_models.Transaction{},
+					},
+				},
+				errors.New("test error"),
+			)
+
+			resp, err := plg.FetchNextPayments(ctx, req)
+			Expect(err).ToNot(BeNil())
+			Expect(err).To(MatchError("test error"))
+			Expect(resp).To(Equal(models.FetchNextPaymentsResponse{}))
+		})
+
+		It("should fetch next payments - no state no results", func(ctx SpecContext) {
+			req := models.FetchNextPaymentsRequest{
+				PageSize: 60,
+			}
+
+			m.EXPECT().GetV1Transactions(gomock.Any(), "", int64(60)).Return(
+				&transactions.GetV1TransactionsOK{
+					Payload: &transactions.GetV1TransactionsOKBody{
+						QueryResponse: atlar_models.QueryResponse{
+							NextToken: "",
+						},
+						Items: []*atlar_models.Transaction{},
+					},
+				},
+				nil,
+			)
+
+			resp, err := plg.FetchNextPayments(ctx, req)
+			Expect(err).To(BeNil())
+			Expect(resp.Payments).To(HaveLen(0))
+			Expect(resp.HasMore).To(BeFalse())
+			Expect(resp.NewState).ToNot(BeNil())
+
+			var state accountsState
+			err = json.Unmarshal(resp.NewState, &state)
+			Expect(err).To(BeNil())
+			// We fetched everything, state should be resetted
+			Expect(state.NextToken).To(BeEmpty())
+		})
+
+		It("should fetch next payments - no state pageSize > total payments", func(ctx SpecContext) {
+			req := models.FetchNextPaymentsRequest{
+				PageSize: 60,
+			}
+
+			m.EXPECT().GetV1Transactions(gomock.Any(), "", int64(60)).Return(
+				&transactions.GetV1TransactionsOK{
+					Payload: &transactions.GetV1TransactionsOKBody{
+						QueryResponse: atlar_models.QueryResponse{
+							NextToken: "",
+						},
+						Items: samplePayments,
+					},
+				},
+				nil,
+			)
+
+			for _, acc := range samplePayments {
+				m.EXPECT().GetV1AccountsID(gomock.Any(), *acc.Account.ID).Return(
+					&accounts.GetV1AccountsIDOK{
+						Payload: &sampleAccount,
+					},
+					nil,
+				)
+
+				m.EXPECT().GetV1BetaThirdPartiesID(gomock.Any(), sampleAccount.ThirdPartyID).Return(
+					&third_parties.GetV1betaThirdPartiesIDOK{
+						Payload: &atlar_models.ThirdParty{
+							ID:   "test",
+							Name: "test",
+						},
+					},
+					nil,
+				)
+			}
+
+			resp, err := plg.FetchNextPayments(ctx, req)
+			Expect(err).To(BeNil())
+			Expect(resp.Payments).To(HaveLen(50))
+			Expect(resp.HasMore).To(BeFalse())
+			Expect(resp.NewState).ToNot(BeNil())
+
+			var state accountsState
+			err = json.Unmarshal(resp.NewState, &state)
+			Expect(err).To(BeNil())
+			// We fetched everything, state should be resetted
+			Expect(state.NextToken).To(BeEmpty())
+		})
+
+		It("should fetch next accounts - no state pageSize < total accounts", func(ctx SpecContext) {
+			req := models.FetchNextPaymentsRequest{
+				PageSize: 40,
+			}
+
+			m.EXPECT().GetV1Transactions(gomock.Any(), "", int64(40)).Return(
+				&transactions.GetV1TransactionsOK{
+					Payload: &transactions.GetV1TransactionsOKBody{
+						QueryResponse: atlar_models.QueryResponse{
+							NextToken: "1234",
+						},
+						Items: samplePayments[:40],
+					},
+				},
+				nil,
+			)
+
+			for _, acc := range samplePayments[:40] {
+				m.EXPECT().GetV1AccountsID(gomock.Any(), *acc.Account.ID).Return(
+					&accounts.GetV1AccountsIDOK{
+						Payload: &sampleAccount,
+					},
+					nil,
+				)
+
+				m.EXPECT().GetV1BetaThirdPartiesID(gomock.Any(), sampleAccount.ThirdPartyID).Return(
+					&third_parties.GetV1betaThirdPartiesIDOK{
+						Payload: &atlar_models.ThirdParty{
+							ID:   "test",
+							Name: "test",
+						},
+					},
+					nil,
+				)
+			}
+
+			resp, err := plg.FetchNextPayments(ctx, req)
+			Expect(err).To(BeNil())
+			Expect(resp.Payments).To(HaveLen(40))
+			Expect(resp.HasMore).To(BeTrue())
+			Expect(resp.NewState).ToNot(BeNil())
+
+			var state accountsState
+			err = json.Unmarshal(resp.NewState, &state)
+			Expect(err).To(BeNil())
+			Expect(state.NextToken).To(Equal("1234"))
+		})
+
+		It("should fetch next accounts - with state pageSize < total accounts", func(ctx SpecContext) {
+			req := models.FetchNextPaymentsRequest{
+				State:    []byte(`{"nextToken": "1234"}`),
+				PageSize: 40,
+			}
+
+			m.EXPECT().GetV1Transactions(gomock.Any(), "1234", int64(40)).Return(
+				&transactions.GetV1TransactionsOK{
+					Payload: &transactions.GetV1TransactionsOKBody{
+						QueryResponse: atlar_models.QueryResponse{
+							NextToken: "",
+						},
+						Items: samplePayments[40:],
+					},
+				},
+				nil,
+			)
+
+			for _, acc := range samplePayments[40:] {
+				m.EXPECT().GetV1AccountsID(gomock.Any(), *acc.Account.ID).Return(
+					&accounts.GetV1AccountsIDOK{
+						Payload: &sampleAccount,
+					},
+					nil,
+				)
+
+				m.EXPECT().GetV1BetaThirdPartiesID(gomock.Any(), sampleAccount.ThirdPartyID).Return(
+					&third_parties.GetV1betaThirdPartiesIDOK{
+						Payload: &atlar_models.ThirdParty{
+							ID:   "test",
+							Name: "test",
+						},
+					},
+					nil,
+				)
+			}
+
+			resp, err := plg.FetchNextPayments(ctx, req)
+			Expect(err).To(BeNil())
+			Expect(resp.Payments).To(HaveLen(10))
+			Expect(resp.HasMore).To(BeFalse())
+			Expect(resp.NewState).ToNot(BeNil())
+
+			var state accountsState
+			err = json.Unmarshal(resp.NewState, &state)
+			Expect(err).To(BeNil())
+			// We fetched everything, state should be resetted
+			Expect(state.NextToken).To(BeEmpty())
+		})
+	})
+})
---
+++ internal/connectors/plugins/public/atlar/plugin_test.go
@@ -0,0 +1,173 @@
+package atlar
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/formancehq/payments/internal/connectors/plugins"
+	"github.com/formancehq/payments/internal/models"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestPlugin(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Atlar Plugin Suite")
+}
+
+var _ = Describe("Atlar Plugin", func() {
+	var (
+		plg *Plugin
+	)
+
+	BeforeEach(func() {
+		plg = &Plugin{}
+	})
+
+	Context("install", func() {
+		It("should report errors in config - baseURL", func(ctx SpecContext) {
+			req := models.InstallRequest{Config: json.RawMessage(`{"accessKey": "test", "secret": "test"}`)}
+			_, err := plg.Install(ctx, req)
+			Expect(err).To(MatchError("missing baseURL in config: invalid config"))
+		})
+
+		It("should report errors in config - accessKey", func(ctx SpecContext) {
+			req := models.InstallRequest{Config: json.RawMessage(`{"baseURL": "test", "secret": "test"}`)}
+			_, err := plg.Install(ctx, req)
+			Expect(err).To(MatchError("missing access key in config: invalid config"))
+		})
+
+		It("should report errors in config - secret", func(ctx SpecContext) {
+			req := models.InstallRequest{Config: json.RawMessage(`{"baseURL": "test", "accessKey": "test"}`)}
+			_, err := plg.Install(ctx, req)
+			Expect(err).To(MatchError("missing secret in config: invalid config"))
+		})
+
+		It("should return valid install response", func(ctx SpecContext) {
+			req := models.InstallRequest{Config: json.RawMessage(`{"baseURL": "http://localhost:8080/", "accessKey": "test", "secret": "test"}`)}
+			res, err := plg.Install(ctx, req)
+			Expect(err).To(BeNil())
+			Expect(len(res.Capabilities) > 0).To(BeTrue())
+			Expect(res.Capabilities).To(Equal(capabilities))
+			Expect(len(res.Workflow) > 0).To(BeTrue())
+			Expect(res.Workflow).To(Equal(workflow()))
+		})
+	})
+
+	Context("uninstall", func() {
+		It("should return valid uninstall response", func(ctx SpecContext) {
+			req := models.UninstallRequest{ConnectorID: "test"}
+			resp, err := plg.Uninstall(ctx, req)
+			Expect(err).To(BeNil())
+			Expect(resp).To(Equal(models.UninstallResponse{}))
+		})
+	})
+
+	Context("fetch next accounts", func() {
+		It("should fail when called before install", func(ctx SpecContext) {
+			req := models.FetchNextAccountsRequest{State: json.RawMessage(`{}`)}
+			_, err := plg.FetchNextAccounts(ctx, req)
+			Expect(err).To(MatchError(plugins.ErrNotYetInstalled))
+		})
+
+		// Other tests will be in accounts_test.go
+	})
+
+	Context("fetch next balances", func() {
+		It("should fail when called before install", func(ctx SpecContext) {
+			req := models.FetchNextBalancesRequest{State: json.RawMessage(`{}`)}
+			_, err := plg.FetchNextBalances(ctx, req)
+			Expect(err).To(MatchError(plugins.ErrNotYetInstalled))
+		})
+
+		// Other tests will be in balances_test.go
+	})
+
+	Context("fetch next external accounts", func() {
+		It("should fail when called before install", func(ctx SpecContext) {
+			req := models.FetchNextExternalAccountsRequest{State: json.RawMessage(`{}`)}
+			_, err := plg.FetchNextExternalAccounts(ctx, req)
+			Expect(err).To(MatchError(plugins.ErrNotYetInstalled))
+		})
+
+		// Other tests will be in external_accounts_test.go
+	})
+
+	Context("fetch next payments", func() {
+		It("should fail when called before install", func(ctx SpecContext) {
+			req := models.FetchNextPaymentsRequest{State: json.RawMessage(`{}`)}
+			_, err := plg.FetchNextPayments(ctx, req)
+			Expect(err).To(MatchError(plugins.ErrNotYetInstalled))
+		})
+
+		// Other tests will be in payments_test.go
+	})
+
+	Context("fetch next others", func() {
+		It("should fail because not implemented", func(ctx SpecContext) {
+			req := models.FetchNextOthersRequest{}
+			_, err := plg.FetchNextOthers(ctx, req)
+			Expect(err).To(MatchError(plugins.ErrNotImplemented))
+		})
+	})
+
+	Context("create bank account", func() {
+		It("should fail when called before install", func(ctx SpecContext) {
+			req := models.CreateBankAccountRequest{}
+			_, err := plg.CreateBankAccount(ctx, req)
+			Expect(err).To(MatchError(plugins.ErrNotYetInstalled))
+		})
+
+		// Other tests will be in bank_accounts_test.go
+	})
+
+	Context("create transfer", func() {
+		It("should fail because not implemented", func(ctx SpecContext) {
+			req := models.CreateTransferRequest{}
+			_, err := plg.CreateTransfer(ctx, req)
+			Expect(err).To(MatchError(plugins.ErrNotImplemented))
+		})
+	})
+
+	Context("poll transfer status", func() {
+		It("should fail because not implemented", func(ctx SpecContext) {
+			req := models.PollTransferStatusRequest{}
+			_, err := plg.PollTransferStatus(ctx, req)
+			Expect(err).To(MatchError(plugins.ErrNotImplemented))
+		})
+	})
+
+	Context("create payout", func() {
+		It("should fail when called before install", func(ctx SpecContext) {
+			req := models.CreatePayoutRequest{}
+			_, err := plg.CreatePayout(ctx, req)
+			Expect(err).To(MatchError(plugins.ErrNotYetInstalled))
+		})
+
+		// Other tests will be in payouts_test.go
+	})
+
+	Context("poll payout status", func() {
+		It("should fail when called before install", func(ctx SpecContext) {
+			req := models.PollPayoutStatusRequest{}
+			_, err := plg.PollPayoutStatus(ctx, req)
+			Expect(err).To(MatchError(plugins.ErrNotYetInstalled))
+		})
+	})
+
+	Context("create webhooks", func() {
+		It("should fail because not implemented", func(ctx SpecContext) {
+			req := models.CreateWebhooksRequest{}
+			_, err := plg.CreateWebhooks(ctx, req)
+			Expect(err).To(MatchError(plugins.ErrNotImplemented))
+		})
+	})
+
+	Context("translate webhook", func() {
+		It("should fail because not implemented", func(ctx SpecContext) {
+			req := models.TranslateWebhookRequest{}
+			_, err := plg.TranslateWebhook(ctx, req)
+			Expect(err).To(MatchError(plugins.ErrNotImplemented))
+		})
+	})
+})
