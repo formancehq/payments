cc584e3493027013a5f7b645517ee5794558b4d6
Crimson Thompson <crimson@formance.com>
2024-11-15 17:31:26 +0100

    test(e2e): bank account forward

--- test/e2e/api_bank_accounts_create_test.go
+++ test/e2e/api_bank_accounts_create_test.go
@@ -3,13 +3,15 @@
 package test_suite
 
 import (
+	"fmt"
+
 	"github.com/formancehq/go-libs/v2/logging"
+	"github.com/formancehq/go-libs/v2/testing/utils"
 	v2 "github.com/formancehq/payments/internal/api/v2"
 	v3 "github.com/formancehq/payments/internal/api/v3"
 	"github.com/formancehq/payments/internal/models"
 	"github.com/google/uuid"
 
-	. "github.com/formancehq/go-libs/v2/testing/utils"
 	"github.com/formancehq/payments/pkg/testserver"
 	. "github.com/formancehq/payments/pkg/testserver"
 	. "github.com/onsi/ginkgo/v2"
@@ -21,36 +23,34 @@ var _ = Context("Payments API Bank Accounts", func() {
 		db  = UseTemplatedDatabase()
 		ctx = logging.TestingContext()
 
-		temporalServer *testserver.TemporalServer
-		app            *Deferred[*testserver.Server]
-
 		accountNumber               = "123456789"
 		iban                        = "DE89370400440532013000"
 		bankAccountsCreateRequest   v3.BankAccountsCreateRequest
 		bankAccountsV2CreateRequest v2.BankAccountsCreateRequest
+
+		app *utils.Deferred[*testserver.Server]
 	)
-	temporalServer = testserver.CreateTemporalServer(GinkgoT())
+
 	app = testserver.NewTestServer(func() Configuration {
 		return Configuration{
+			Stack:                 stack,
 			PostgresConfiguration: db.GetValue().ConnectionOptions(),
-			TemporalNamespace:     temporalServer.GetDefaultNamespace(),
-			TemporalAddress:       temporalServer.Address(),
+			TemporalNamespace:     temporalServer.GetValue().DefaultNamespace(),
+			TemporalAddress:       temporalServer.GetValue().Address(),
 			Output:                GinkgoWriter,
 		}
 	})
 
-	BeforeEach(func() {
-		bankAccountsCreateRequest = v3.BankAccountsCreateRequest{
-			Name:          "foo",
-			AccountNumber: &accountNumber,
-			IBAN:          &iban,
-		}
-		bankAccountsV2CreateRequest = v2.BankAccountsCreateRequest{
-			Name:          "foo",
-			AccountNumber: &accountNumber,
-			IBAN:          &iban,
-		}
-	})
+	bankAccountsCreateRequest = v3.BankAccountsCreateRequest{
+		Name:          "foo",
+		AccountNumber: &accountNumber,
+		IBAN:          &iban,
+	}
+	bankAccountsV2CreateRequest = v2.BankAccountsCreateRequest{
+		Name:          "foo",
+		AccountNumber: &accountNumber,
+		IBAN:          &iban,
+	}
 
 	When("creating a new bank account with v3", func() {
 		var (
@@ -94,42 +94,59 @@ var _ = Context("Payments API Bank Accounts", func() {
 
 	When("forwarding a bank account to a connector with v3", func() {
 		var (
-			ver                        int
-			bankAccountsCreateResponse struct{ Data string }
-			forwardReq                 v3.BankAccountsForwardToConnectorRequest
-			res                        struct{ Data models.Task }
-			err                        error
-			id                         uuid.UUID
+			ver                   int
+			bankAccountsCreateRes struct{ Data string }
+			connectorRes          struct{ Data string }
+			connectorConf         ConnectorConf
+			forwardReq            v3.BankAccountsForwardToConnectorRequest
+			res                   struct{ Data models.Task }
+			err                   error
+			id                    uuid.UUID
 		)
 		JustBeforeEach(func() {
 			ver = 3
-			err = CreateBankAccount(ctx, app.GetValue(), ver, bankAccountsCreateRequest, &bankAccountsCreateResponse)
+			err = CreateBankAccount(ctx, app.GetValue(), ver, bankAccountsCreateRequest, &bankAccountsCreateRes)
 			Expect(err).To(BeNil())
-			id, err = uuid.Parse(bankAccountsCreateResponse.Data)
+			id, err = uuid.Parse(bankAccountsCreateRes.Data)
+			Expect(err).To(BeNil())
+			connectorConf = ConnectorConf{
+				Name:          fmt.Sprintf("connector-%s", id.String()),
+				PollingPeriod: "2m",
+				PageSize:      30,
+				APIKey:        "key",
+				Endpoint:      "http://example.com",
+			}
+			err := InstallConnector(ctx, app.GetValue(), ver, connectorConf, &connectorRes)
 			Expect(err).To(BeNil())
 		})
+
 		It("should should fail when connector ID is invalid", func() {
 			forwardReq = v3.BankAccountsForwardToConnectorRequest{ConnectorID: "invalid"}
 			err = ForwardBankAccount(ctx, app.GetValue(), ver, id.String(), &forwardReq, &res)
 			Expect(err).NotTo(BeNil())
 			Expect(err.Error()).To(ContainSubstring("400"))
 		})
+		It("should should be ok when connector is installed", func() {
+			forwardReq = v3.BankAccountsForwardToConnectorRequest{ConnectorID: connectorRes.Data}
+			err = ForwardBankAccount(ctx, app.GetValue(), ver, id.String(), &forwardReq, &res)
+			Expect(err).To(BeNil())
+		})
 	})
 
 	When("forwarding a bank account to a connector with v2", func() {
 		var (
-			ver                        int
-			bankAccountsCreateResponse struct{ Data v2.BankAccountResponse }
-			forwardReq                 v2.BankAccountsForwardToConnectorRequest
-			res                        struct{ Data models.Task }
-			err                        error
-			id                         uuid.UUID
+			ver                   int
+			bankAccountsCreateRes struct{ Data v2.BankAccountResponse }
+			forwardReq            v2.BankAccountsForwardToConnectorRequest
+			res                   struct{ Data models.Task }
+			err                   error
+			id                    uuid.UUID
 		)
 		JustBeforeEach(func() {
 			ver = 2
-			err = CreateBankAccount(ctx, app.GetValue(), ver, bankAccountsCreateRequest, &bankAccountsCreateResponse)
+			err = CreateBankAccount(ctx, app.GetValue(), ver, bankAccountsCreateRequest, &bankAccountsCreateRes)
 			Expect(err).To(BeNil())
-			id, err = uuid.Parse(bankAccountsCreateResponse.Data.ID)
+			id, err = uuid.Parse(bankAccountsCreateRes.Data.ID)
 			Expect(err).To(BeNil())
 		})
 		It("should should fail when connector ID is invalid", func() {
