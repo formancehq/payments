55335cc97c5895a515cb6f718d8e1d3dc85aa352
Paul Nicolas <paul.nicolas.go@gmail.com>
2024-11-18 00:22:29 +0100

    feat(temporal): continue as new when fetching too much data

--- internal/connectors/engine/workflow/fetch_payments.go
+++ internal/connectors/engine/workflow/fetch_payments.go
@@ -189,6 +189,18 @@ func (w Workflow) fetchNextPayments(
 		}
 
 		hasMore = paymentsResponse.HasMore
+
+		if w.shouldContinueAsNew(ctx) {
+			// If we have lots and lots of accounts, sometimes, we need to
+			// continue as new to not exeed the maximum history size or length
+			// of a workflow.
+			return workflow.NewContinueAsNewError(
+				ctx,
+				RunFetchNextPayments,
+				fetchNextPayments,
+				nextTasks,
+			)
+		}
 	}
 
 	return nil
