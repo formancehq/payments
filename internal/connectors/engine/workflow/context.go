f99eda9bfbc9968199db96d7e07dbd0bf08cfb56
Crimson Thompson <crimson@formance.com>
2024-11-20 15:47:52 +0100

    fix: disable retry of connector install when plugin cannot boot correctly

--- internal/connectors/engine/workflow/context.go
+++ internal/connectors/engine/workflow/context.go
@@ -24,3 +24,18 @@ func infiniteRetryContext(ctx workflow.Context) workflow.Context {
 		},
 	})
 }
+
+func maximumAttemptsRetryContext(ctx workflow.Context, attempts int) workflow.Context {
+	return workflow.WithActivityOptions(ctx, workflow.ActivityOptions{
+		StartToCloseTimeout: 60 * time.Second,
+		RetryPolicy: &temporal.RetryPolicy{
+			MaximumAttempts:    int32(attempts),
+			InitialInterval:    time.Second,
+			BackoffCoefficient: 2,
+			MaximumInterval:    100 * time.Second,
+			NonRetryableErrorTypes: []string{
+				ErrorCodeValidation,
+			},
+		},
+	})
+}
