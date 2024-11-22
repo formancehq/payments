a5618253e1e5bdddb240515a12c195ec125a8da4
Crimson Thompson <crimson@formance.com>
2024-11-22 15:53:20 +0100

    feat(profiling): enable profiling routes on 9090 by default

--- cmd/root.go
+++ cmd/root.go
@@ -16,6 +16,7 @@ import (
 	"github.com/formancehq/go-libs/v2/logging"
 	"github.com/formancehq/go-libs/v2/otlp"
 	"github.com/formancehq/go-libs/v2/otlp/otlptraces"
+	"github.com/formancehq/go-libs/v2/profiling"
 	"github.com/formancehq/go-libs/v2/publish"
 	"github.com/formancehq/go-libs/v2/service"
 	"github.com/formancehq/go-libs/v2/temporal"
@@ -28,8 +29,6 @@ import (
 	"go.uber.org/fx"
 )
 
-// TODO(polo/crimson): add profiling
-
 var (
 	ServiceName = "payments"
 	Version     = "develop"
@@ -41,6 +40,8 @@ const (
 	PluginsDirectoryPathFlag = "plugin-directory-path"
 	ConfigEncryptionKeyFlag  = "config-encryption-key"
 	ListenFlag               = "listen"
+	ListenProfilerFlag       = "listen-profiler"
+	EnableProfilerFlag       = "enable-profiler"
 	stackFlag                = "stack"
 	stackPublicURLFlag       = "stack-public-url"
 )
@@ -64,6 +65,8 @@ func NewRootCommand() *cobra.Command {
 	server := newServer()
 	addAutoMigrateCommand(server)
 	server.Flags().String(ListenFlag, ":8080", "Listen address")
+	server.Flags().String(ListenProfilerFlag, ":9090", "Pprof listen address")
+	server.Flags().Bool(EnableProfilerFlag, true, "Whether or not to enable pprof endpoints")
 	server.Flags().String(PluginsDirectoryPathFlag, "", "Plugin directory path")
 	server.Flags().String(stackFlag, "", "Stack name")
 	server.Flags().String(stackPublicURLFlag, "", "Stack public url")
@@ -105,6 +108,8 @@ func commonOptions(cmd *cobra.Command) (fx.Option, error) {
 	}
 
 	listen, _ := cmd.Flags().GetString(ListenFlag)
+	listenProfiler, _ := cmd.Flags().GetString(ListenProfilerFlag)
+	enableProfiler, _ := cmd.Flags().GetBool(EnableProfilerFlag)
 	stack, _ := cmd.Flags().GetString(stackFlag)
 	stackPublicURL, _ := cmd.Flags().GetString(stackPublicURLFlag)
 	debug, _ := cmd.Flags().GetBool(service.DebugFlag)
@@ -141,6 +146,7 @@ func commonOptions(cmd *cobra.Command) (fx.Option, error) {
 		licence.FXModuleFromFlags(cmd, ServiceName),
 		storage.Module(cmd, *connectionOptions, configEncryptionKey),
 		api.NewModule(listen, service.IsDebug(cmd)),
+		profiling.NewModule(listenProfiler, enableProfiler),
 		engine.Module(pluginPaths, stack, stackPublicURL, temporalNamespace, rawFlags, debug, jsonFormatter),
 		v2.NewModule(),
 		v3.NewModule(),
