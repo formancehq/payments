3725cda87ae8fcd5c32c33a9361421cecc7600bb
Paul Nicolas <paul.nicolas.go@gmail.com>
2024-12-09 17:06:07 +0100

    add on conflict everywhere

--- internal/storage/migrations/4-migrate-bank-accounts-from-v2.sql
+++ internal/storage/migrations/4-migrate-bank-accounts-from-v2.sql
@@ -3,7 +3,8 @@ DO $$
         IF (SELECT count(*) FROM information_schema.tables WHERE table_schema ='accounts' AND table_name ='bank_account') > 0
         THEN
             INSERT INTO bank_accounts (id, created_at, name, country, account_number, iban, swift_bic_code, metadata)
-            SELECT id, created_at, name, country, account_number, iban, swift_bic_code, metadata from accounts.bank_account;
+            SELECT id, created_at, name, country, account_number, iban, swift_bic_code, metadata from accounts.bank_account
+            On CONFLICT (id) DO NOTHING;
         END IF;
     END;
 $$;
@@ -13,7 +14,8 @@ DO $$
         IF (SELECT count(*) FROM information_schema.tables WHERE table_schema ='accounts' AND table_name ='bank_account_related_accounts') > 0
         THEN
             INSERT INTO bank_accounts_related_accounts (bank_account_id, account_id, connector_id, created_at)
-            SELECT id, account_id, connector_id, created_at from accounts.bank_account_related_accounts;
+            SELECT id, account_id, connector_id, created_at from accounts.bank_account_related_accounts
+            ON CONFLICT (bank_account_id, account_id) DO NOTHING;
         END IF;
     END;
 $$;
