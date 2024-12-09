3a489965effb9dc9975f0b815a885e223c2d2b69
Paul Nicolas <paul.nicolas.go@gmail.com>
2024-12-09 17:06:07 +0100

    add on conflict everywhere

--- internal/storage/migrations/5-migrate-transfer-initiations-from-v2.sql
+++ internal/storage/migrations/5-migrate-transfer-initiations-from-v2.sql
@@ -3,7 +3,8 @@ DO $$
         IF (SELECT count(*) FROM information_schema.tables WHERE table_schema ='transfers' AND table_name ='transfer_initiation') > 0
         THEN
             INSERT INTO payment_initiations (id, connector_id, reference, created_at, scheduled_at, description, type, amount, asset, source_account_id, destination_account_id, metadata)
-            SELECT id, connector_id, reference, created_at, COALESCE(scheduled_at, '0001-01-01 00:00:00+00'::timestamp without time zone) as scheduled_at, description, type+1 as type, amount, asset, source_account_id, destination_account_id, metadata from transfers.transfer_initiation;
+            SELECT id, connector_id, reference, created_at, COALESCE(scheduled_at, '0001-01-01 00:00:00+00'::timestamp without time zone) as scheduled_at, description, type+1 as type, amount, asset, source_account_id, destination_account_id, metadata from transfers.transfer_initiation
+            ON CONFLICT (id) DO NOTHING;
         END IF;
     END;
 $$;
@@ -13,7 +14,8 @@ DO $$
         IF (SELECT count(*) FROM information_schema.tables WHERE table_schema ='transfers' AND table_name ='transfer_initiation_payments') > 0
         THEN
             INSERT INTO payment_initiation_related_payments (payment_initiation_id, payment_id, created_at)
-            SELECT transfer_initiation_id, payment_id, created_at from transfers.transfer_initiation_payments;
+            SELECT transfer_initiation_id, payment_id, created_at from transfers.transfer_initiation_payments
+            ON CONFLICT (payment_initiation_id, payment_id) DO NOTHING;
         END IF;
     END;
 $$;
--- internal/storage/migrations/7-migrate-pools-from-v2.sql
+++ internal/storage/migrations/7-migrate-pools-from-v2.sql
@@ -3,7 +3,8 @@ DO $$
         IF (SELECT count(*) FROM information_schema.tables WHERE table_schema ='accounts' AND table_name ='pools') > 0
         THEN
             INSERT INTO pools (id, name, created_at)
-            SELECT id, name, created_at FROM accounts.pools;
+            SELECT id, name, created_at FROM accounts.pools
+            ON CONFLICT (id) DO NOTHING;
         END IF;
     END;
 $$;
@@ -13,7 +14,8 @@ DO $$
         IF (SELECT count(*) FROM information_schema.tables WHERE table_schema ='accounts' AND table_name ='pool_accounts') > 0
         THEN
             INSERT INTO pool_accounts (pool_id, account_id)
-            SELECT pool_id, account_id FROM accounts.pool_accounts;
+            SELECT pool_id, account_id FROM accounts.pool_accounts
+            ON CONFLICT (pool_id, account_id) DO NOTHING;
         END IF;
     END;
 $$;
