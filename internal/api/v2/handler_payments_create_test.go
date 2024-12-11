1d92662b67f000bfdb64fcd8e03cffc376a0fe1f
Crimson Thompson <crimson@formance.com>
2024-12-11 19:05:50 +0100

    test: (e2e) make payment creation request structs public

--- internal/api/v2/handler_payments_create_test.go
+++ internal/api/v2/handler_payments_create_test.go
@@ -27,7 +27,7 @@ var _ = Describe("API v2 Payments Create", func() {
 		var (
 			w   *httptest.ResponseRecorder
 			m   *backend.MockBackend
-			cpr createPaymentRequest
+			cpr CreatePaymentRequest
 		)
 		BeforeEach(func() {
 			w = httptest.NewRecorder()
@@ -44,25 +44,25 @@ var _ = Describe("API v2 Payments Create", func() {
 		})
 
 		DescribeTable("validation errors",
-			func(cpr createPaymentRequest) {
+			func(cpr CreatePaymentRequest) {
 				handlerFn(w, prepareJSONRequest(http.MethodPost, &cpr))
 				assertExpectedResponse(w.Result(), http.StatusBadRequest, ErrValidation)
 			},
-			Entry("reference missing", createPaymentRequest{}),
-			Entry("connectorID missing", createPaymentRequest{Reference: "ref"}),
-			Entry("createdAt missing", createPaymentRequest{Reference: "ref", ConnectorID: "id"}),
-			Entry("amount missing", createPaymentRequest{Reference: "ref", ConnectorID: "id", CreatedAt: time.Now()}),
-			Entry("payment type missing", createPaymentRequest{Reference: "ref", ConnectorID: "id", CreatedAt: time.Now(), Amount: big.NewInt(4467)}),
-			Entry("payment type invalid", createPaymentRequest{Reference: "ref", ConnectorID: "id", CreatedAt: time.Now(), Amount: big.NewInt(4467), Type: "invalid"}),
-			Entry("scheme missing", createPaymentRequest{Reference: "ref", ConnectorID: "id", CreatedAt: time.Now(), Amount: big.NewInt(4467), Type: "PAYOUT"}),
-			Entry("scheme invalid", createPaymentRequest{Reference: "ref", ConnectorID: "id", CreatedAt: time.Now(), Amount: big.NewInt(4467), Type: "PAYOUT", Scheme: "invalid"}),
-			Entry("asset missing", createPaymentRequest{Reference: "ref", ConnectorID: "id", CreatedAt: time.Now(), Amount: big.NewInt(4467), Type: "PAYOUT", Scheme: "CARD_VISA"}),
+			Entry("reference missing", CreatePaymentRequest{}),
+			Entry("connectorID missing", CreatePaymentRequest{Reference: "ref"}),
+			Entry("createdAt missing", CreatePaymentRequest{Reference: "ref", ConnectorID: "id"}),
+			Entry("amount missing", CreatePaymentRequest{Reference: "ref", ConnectorID: "id", CreatedAt: time.Now()}),
+			Entry("payment type missing", CreatePaymentRequest{Reference: "ref", ConnectorID: "id", CreatedAt: time.Now(), Amount: big.NewInt(4467)}),
+			Entry("payment type invalid", CreatePaymentRequest{Reference: "ref", ConnectorID: "id", CreatedAt: time.Now(), Amount: big.NewInt(4467), Type: "invalid"}),
+			Entry("scheme missing", CreatePaymentRequest{Reference: "ref", ConnectorID: "id", CreatedAt: time.Now(), Amount: big.NewInt(4467), Type: "PAYOUT"}),
+			Entry("scheme invalid", CreatePaymentRequest{Reference: "ref", ConnectorID: "id", CreatedAt: time.Now(), Amount: big.NewInt(4467), Type: "PAYOUT", Scheme: "invalid"}),
+			Entry("asset missing", CreatePaymentRequest{Reference: "ref", ConnectorID: "id", CreatedAt: time.Now(), Amount: big.NewInt(4467), Type: "PAYOUT", Scheme: "CARD_VISA"}),
 		)
 
 		It("should return an internal server error when backend returns error", func(ctx SpecContext) {
 			expectedErr := errors.New("payment create err")
 			m.EXPECT().PaymentsCreate(gomock.Any(), gomock.Any()).Return(expectedErr)
-			cpr = createPaymentRequest{
+			cpr = CreatePaymentRequest{
 				Reference:   "reference-err",
 				ConnectorID: connID.String(),
 				CreatedAt:   time.Now(),
@@ -78,7 +78,7 @@ var _ = Describe("API v2 Payments Create", func() {
 
 		It("should return status OK on success", func(ctx SpecContext) {
 			m.EXPECT().PaymentsCreate(gomock.Any(), gomock.Any()).Return(nil)
-			cpr = createPaymentRequest{
+			cpr = CreatePaymentRequest{
 				Reference:   "reference-ok",
 				ConnectorID: connID.String(),
 				CreatedAt:   time.Now(),
--- internal/api/v3/handler_payments_create_test.go
+++ internal/api/v3/handler_payments_create_test.go
@@ -27,8 +27,8 @@ var _ = Describe("API v3 Payments Create", func() {
 		var (
 			w   *httptest.ResponseRecorder
 			m   *backend.MockBackend
-			cpr createPaymentRequest
-			adj []createPaymentsAdjustmentsRequest
+			cpr CreatePaymentRequest
+			adj []CreatePaymentsAdjustmentsRequest
 		)
 		BeforeEach(func() {
 			w = httptest.NewRecorder()
@@ -37,7 +37,7 @@ var _ = Describe("API v3 Payments Create", func() {
 			handlerFn = paymentsCreate(m)
 
 			asset := "JPY"
-			adj = []createPaymentsAdjustmentsRequest{
+			adj = []CreatePaymentsAdjustmentsRequest{
 				{
 					Reference: "ref_adjustment",
 					CreatedAt: time.Now(),
@@ -56,26 +56,26 @@ var _ = Describe("API v3 Payments Create", func() {
 		})
 
 		DescribeTable("validation errors",
-			func(cpr createPaymentRequest) {
+			func(cpr CreatePaymentRequest) {
 				handlerFn(w, prepareJSONRequest(http.MethodPost, &cpr))
 				assertExpectedResponse(w.Result(), http.StatusBadRequest, ErrValidation)
 			},
-			Entry("reference missing", createPaymentRequest{}),
-			Entry("connectorID missing", createPaymentRequest{Reference: "ref"}),
-			Entry("createdAt missing", createPaymentRequest{Reference: "ref", ConnectorID: "id"}),
-			Entry("amount missing", createPaymentRequest{Reference: "ref", ConnectorID: "id", CreatedAt: time.Now()}),
-			Entry("payment type missing", createPaymentRequest{Reference: "ref", ConnectorID: "id", CreatedAt: time.Now(), Amount: big.NewInt(4467)}),
-			Entry("payment type invalid", createPaymentRequest{Reference: "ref", ConnectorID: "id", CreatedAt: time.Now(), Amount: big.NewInt(4467), Type: "invalid"}),
-			Entry("scheme missing", createPaymentRequest{Reference: "ref", ConnectorID: "id", CreatedAt: time.Now(), Amount: big.NewInt(4467), Type: "PAYOUT"}),
-			Entry("scheme invalid", createPaymentRequest{Reference: "ref", ConnectorID: "id", CreatedAt: time.Now(), Amount: big.NewInt(4467), Type: "PAYOUT", Scheme: "invalid"}),
-			Entry("asset missing", createPaymentRequest{Reference: "ref", ConnectorID: "id", CreatedAt: time.Now(), Amount: big.NewInt(4467), Type: "PAYOUT", Scheme: "CARD_VISA"}),
-			Entry("adjustments missing", createPaymentRequest{Reference: "ref", ConnectorID: "id", CreatedAt: time.Now(), Amount: big.NewInt(4467), Type: "PAYOUT", Scheme: "CARD_VISA", Asset: "CAD"}),
+			Entry("reference missing", CreatePaymentRequest{}),
+			Entry("connectorID missing", CreatePaymentRequest{Reference: "ref"}),
+			Entry("createdAt missing", CreatePaymentRequest{Reference: "ref", ConnectorID: "id"}),
+			Entry("amount missing", CreatePaymentRequest{Reference: "ref", ConnectorID: "id", CreatedAt: time.Now()}),
+			Entry("payment type missing", CreatePaymentRequest{Reference: "ref", ConnectorID: "id", CreatedAt: time.Now(), Amount: big.NewInt(4467)}),
+			Entry("payment type invalid", CreatePaymentRequest{Reference: "ref", ConnectorID: "id", CreatedAt: time.Now(), Amount: big.NewInt(4467), Type: "invalid"}),
+			Entry("scheme missing", CreatePaymentRequest{Reference: "ref", ConnectorID: "id", CreatedAt: time.Now(), Amount: big.NewInt(4467), Type: "PAYOUT"}),
+			Entry("scheme invalid", CreatePaymentRequest{Reference: "ref", ConnectorID: "id", CreatedAt: time.Now(), Amount: big.NewInt(4467), Type: "PAYOUT", Scheme: "invalid"}),
+			Entry("asset missing", CreatePaymentRequest{Reference: "ref", ConnectorID: "id", CreatedAt: time.Now(), Amount: big.NewInt(4467), Type: "PAYOUT", Scheme: "CARD_VISA"}),
+			Entry("adjustments missing", CreatePaymentRequest{Reference: "ref", ConnectorID: "id", CreatedAt: time.Now(), Amount: big.NewInt(4467), Type: "PAYOUT", Scheme: "CARD_VISA", Asset: "CAD"}),
 		)
 
 		It("should return an internal server error when backend returns error", func(ctx SpecContext) {
 			expectedErr := errors.New("payment create err")
 			m.EXPECT().PaymentsCreate(gomock.Any(), gomock.Any()).Return(expectedErr)
-			cpr = createPaymentRequest{
+			cpr = CreatePaymentRequest{
 				Reference:   "reference-err",
 				ConnectorID: connID.String(),
 				CreatedAt:   time.Now(),
@@ -91,7 +91,7 @@ var _ = Describe("API v3 Payments Create", func() {
 
 		It("should return status created on success", func(ctx SpecContext) {
 			m.EXPECT().PaymentsCreate(gomock.Any(), gomock.Any()).Return(nil)
-			cpr = createPaymentRequest{
+			cpr = CreatePaymentRequest{
 				Reference:   "reference-ok",
 				ConnectorID: connID.String(),
 				CreatedAt:   time.Now(),
