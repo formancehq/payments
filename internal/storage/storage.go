2bf36604211e1ae416689752c2a4a173c0498d0b
Paul Nicolas <paul.nicolas.go@gmail.com>
2024-12-12 15:32:20 +0100

    remove unused function

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -20,7 +20,6 @@ type Storage interface {
 	// Accounts
 	AccountsUpsert(ctx context.Context, accounts []models.Account) error
 	AccountsGet(ctx context.Context, id models.AccountID) (*models.Account, error)
-	AccountsExists(ctx context.Context, id models.AccountID) (bool, error)
 	AccountsList(ctx context.Context, q ListAccountsQuery) (*bunpaginate.Cursor[models.Account], error)
 	AccountsDeleteFromConnectorID(ctx context.Context, connectorID models.ConnectorID) error
 
