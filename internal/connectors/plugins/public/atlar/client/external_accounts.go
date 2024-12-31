0f6158641439cf4ce97028a77f824e800086cb22
Crimson Thompson <crimson@formance.com>
2024-12-31 18:44:17 +0100

    fix: (metrics) ensure that atlar metrics are consistent with other connectors

--- internal/connectors/plugins/public/atlar/client/external_accounts.go
+++ internal/connectors/plugins/public/atlar/client/external_accounts.go
@@ -2,18 +2,16 @@ package client
 
 import (
 	"context"
-	"time"
 
+	"github.com/formancehq/payments/internal/connectors/metrics"
 	"github.com/get-momo/atlar-v1-go-client/client/external_accounts"
 )
 
 func (c *client) GetV1ExternalAccountsID(ctx context.Context, externalAccountID string) (*external_accounts.GetV1ExternalAccountsIDOK, error) {
-	start := time.Now()
-	defer c.recordMetrics(ctx, start, "get_external_account")
-
 	getExternalAccountParams := external_accounts.GetV1ExternalAccountsIDParams{
-		Context: ctx,
-		ID:      externalAccountID,
+		Context:    metrics.OperationContext(ctx, "get_external_account"),
+		ID:         externalAccountID,
+		HTTPClient: c.httpClient,
 	}
 
 	externalAccountResponse, err := c.client.ExternalAccounts.GetV1ExternalAccountsID(&getExternalAccountParams)
@@ -21,13 +19,11 @@ func (c *client) GetV1ExternalAccountsID(ctx context.Context, externalAccountID 
 }
 
 func (c *client) GetV1ExternalAccounts(ctx context.Context, token string, pageSize int64) (*external_accounts.GetV1ExternalAccountsOK, error) {
-	start := time.Now()
-	defer c.recordMetrics(ctx, start, "list_external_accounts")
-
 	externalAccountsParams := external_accounts.GetV1ExternalAccountsParams{
-		Limit:   &pageSize,
-		Context: ctx,
-		Token:   &token,
+		Limit:      &pageSize,
+		Context:    metrics.OperationContext(ctx, "list_external_accounts"),
+		Token:      &token,
+		HTTPClient: c.httpClient,
 	}
 
 	resp, err := c.client.ExternalAccounts.GetV1ExternalAccounts(&externalAccountsParams)
--- internal/connectors/plugins/public/atlar/client/transfers.go
+++ internal/connectors/plugins/public/atlar/client/transfers.go
@@ -2,19 +2,17 @@ package client
 
 import (
 	"context"
-	"time"
 
+	"github.com/formancehq/payments/internal/connectors/metrics"
 	"github.com/get-momo/atlar-v1-go-client/client/credit_transfers"
 	atlar_models "github.com/get-momo/atlar-v1-go-client/models"
 )
 
 func (c *client) PostV1CreditTransfers(ctx context.Context, req *atlar_models.CreatePaymentRequest) (*credit_transfers.PostV1CreditTransfersCreated, error) {
-	start := time.Now()
-	defer c.recordMetrics(ctx, start, "create_credit_transfer")
-
 	postCreditTransfersParams := credit_transfers.PostV1CreditTransfersParams{
-		Context:        ctx,
+		Context:        metrics.OperationContext(ctx, "create_credit_transfer"),
 		CreditTransfer: req,
+		HTTPClient:     c.httpClient,
 	}
 
 	resp, err := c.client.CreditTransfers.PostV1CreditTransfers(&postCreditTransfersParams)
@@ -22,12 +20,10 @@ func (c *client) PostV1CreditTransfers(ctx context.Context, req *atlar_models.Cr
 }
 
 func (c *client) GetV1CreditTransfersGetByExternalIDExternalID(ctx context.Context, externalID string) (*credit_transfers.GetV1CreditTransfersGetByExternalIDExternalIDOK, error) {
-	start := time.Now()
-	defer c.recordMetrics(ctx, start, "get_credit_transfer")
-
 	getCreditTransferParams := credit_transfers.GetV1CreditTransfersGetByExternalIDExternalIDParams{
-		Context:    ctx,
+		Context:    metrics.OperationContext(ctx, "get_credit_transfer"),
 		ExternalID: externalID,
+		HTTPClient: c.httpClient,
 	}
 
 	resp, err := c.client.CreditTransfers.GetV1CreditTransfersGetByExternalIDExternalID(&getCreditTransferParams)
