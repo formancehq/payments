6661edbf54e244e150aaa52924946e328cf163f4
Paul Nicolas <paul.nicolas.go@gmail.com>
2025-01-24 11:14:12 +0100

    fix(temporal): fix schedule already running error (#281)
    * fix(temporal): fix schedule already running error
    
    * fix(atlar): missing periodically in atlar connector
    
    * feat(activities): add tests about schedule already running

--- internal/connectors/plugins/public/atlar/workflow.go
+++ internal/connectors/plugins/public/atlar/workflow.go
@@ -19,11 +19,11 @@ func workflow() models.ConnectorTasksTree {
 			},
 		},
 		{
-			TaskType:  models.TASK_FETCH_PAYMENTS,
-			Name:      "fetch_payments",
-			NextTasks: []models.ConnectorTaskTree{},
+			TaskType:     models.TASK_FETCH_PAYMENTS,
+			Name:         "fetch_payments",
+			Periodically: true,
+			NextTasks:    []models.ConnectorTaskTree{},
 		},
-
 		{
 			TaskType:     models.TASK_FETCH_EXTERNAL_ACCOUNTS,
 			Name:         "fetch_external_accounts",
