c479ec411e50498b07393e3917c4adfc63e1117c
Paul Nicolas <paul.nicolas.go@gmail.com>
2025-04-25 12:21:32 +0200

    feat: add psu models

---
+++ internal/models/psu.go
@@ -0,0 +1,97 @@
+package models
+
+import (
+	"encoding/json"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Address struct {
+	StreetName   *string `json:"streetName"`
+	StreetNumber *string `json:"streetNumber"`
+	City         *string `json:"city"`
+	Region       *string `json:"region"`
+	PostalCode   *string `json:"postalCode"`
+	Country      *string `json:"country"`
+}
+
+type ContactDetails struct {
+	Email       *string `json:"email"`
+	PhoneNumber *string `json:"phoneNumber"`
+}
+
+type PaymentServiceUser struct {
+	ID        uuid.UUID `json:"id"`
+	Name      string    `json:"name"`
+	CreatedAt time.Time `json:"createdAt"`
+
+	// Optional fields
+	ContactDetails *ContactDetails   `json:"contactDetails"`
+	Address        *Address          `json:"address"`
+	BankAccountIDs []uuid.UUID       `json:"bankAccountIDs"`
+	Metadata       map[string]string `json:"metadata"`
+}
+
+func (psu PaymentServiceUser) MarshalJSON() ([]byte, error) {
+	return json.Marshal(&struct {
+		ID             string            `json:"id"`
+		Name           string            `json:"name"`
+		CreatedAt      time.Time         `json:"createdAt"`
+		ContactDetails *ContactDetails   `json:"contactDetails"`
+		Address        *Address          `json:"address"`
+		BankAccountIDs []string          `json:"bankAccountIDs"`
+		Metadata       map[string]string `json:"metadata"`
+	}{
+		ID:             psu.ID.String(),
+		Name:           psu.Name,
+		CreatedAt:      psu.CreatedAt,
+		ContactDetails: psu.ContactDetails,
+		Address:        psu.Address,
+		BankAccountIDs: func() []string {
+			if len(psu.BankAccountIDs) == 0 {
+				return nil
+			}
+			bankAccountIDs := make([]string, len(psu.BankAccountIDs))
+			for i, id := range psu.BankAccountIDs {
+				bankAccountIDs[i] = id.String()
+			}
+			return bankAccountIDs
+		}(),
+		Metadata: psu.Metadata,
+	})
+}
+
+func (psu *PaymentServiceUser) UnmarshalJSON(data []byte) error {
+	var aux struct {
+		ID             string            `json:"id"`
+		Name           string            `json:"name"`
+		CreatedAt      time.Time         `json:"createdAt"`
+		ContactDetails *ContactDetails   `json:"contactDetails"`
+		Address        *Address          `json:"address"`
+		BankAccountIDs []string          `json:"bankAccountIDs"`
+		Metadata       map[string]string `json:"metadata"`
+	}
+
+	if err := json.Unmarshal(data, &aux); err != nil {
+		return err
+	}
+
+	psu.ID, _ = uuid.Parse(aux.ID)
+	psu.Name = aux.Name
+	psu.CreatedAt = aux.CreatedAt
+	psu.ContactDetails = aux.ContactDetails
+	psu.Address = aux.Address
+	psu.Metadata = aux.Metadata
+
+	if len(aux.BankAccountIDs) > 0 {
+		psu.BankAccountIDs = make([]uuid.UUID, len(aux.BankAccountIDs))
+		for i, id := range aux.BankAccountIDs {
+			psu.BankAccountIDs[i], _ = uuid.Parse(id)
+		}
+	} else {
+		psu.BankAccountIDs = nil
+	}
+
+	return nil
+}
