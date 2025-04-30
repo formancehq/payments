33150dd540eee6c02fa71086f426527ff8523662
fabrice guery <ecolofou@gmail.com>
2025-04-30 12:01:19 +0200

    feat(CONN-92): fetchBalance implemented

--- internal/connectors/plugins/public/qonto/balances.go
+++ internal/connectors/plugins/public/qonto/balances.go
@@ -2,45 +2,36 @@ package qonto
 
 import (
 	"context"
+	"encoding/json"
+	"github.com/formancehq/payments/internal/connectors/plugins/public/qonto/client"
+	"math/big"
+	"time"
 
 	"github.com/formancehq/payments/internal/models"
 )
 
 func (p *Plugin) fetchNextBalances(ctx context.Context, req models.FetchNextBalancesRequest) (models.FetchNextBalancesResponse, error) {
-	// TODO: if needed, uncomment the following lines to get the related account in request
-	// var from models.PSPAccount
-	// if req.FromPayload == nil {
-	// 	return models.FetchNextBalancesResponse{}, models.ErrMissingFromPayloadInRequest
-	// }
-	// if err := json.Unmarshal(req.FromPayload, &from); err != nil {
-	// 	return models.FetchNextBalancesResponse{}, err
-	// }
-
-	balances, err := p.client.GetAccountBalances(ctx)
-	if err != nil {
+	var from models.PSPAccount
+	if req.FromPayload == nil {
+		return models.FetchNextBalancesResponse{}, models.ErrMissingFromPayloadInRequest
+	}
+	if err := json.Unmarshal(req.FromPayload, &from); err != nil {
 		return models.FetchNextBalancesResponse{}, err
 	}
 
-	var accountBalances []models.PSPBalance
-	for _, balance := range balances {
-		// TODO: You can use the following method to extract the amount and the
-		// asset from the PSP data if you're not already in minor units currency.
-		// precision, err := currency.GetPrecision(supportedCurrenciesWithDecimal, balance.Attributes.CurrencyCode)
-		// if err != nil {
-		// 	return models.FetchNextBalancesResponse{}, err
-		// }
+	var qontoBankAccount client.QontoBankAccount
 
-		// amount, err := currency.GetAmountWithPrecisionFromString(balance.Attributes.AvailableBalance.String(), precision)
-		// if err != nil {
-		// 	return models.FetchNextBalancesResponse{}, err
-		// }
-
-		// asset := currency.FormatAsset(supportedCurrenciesWithDecimal, balance.Attributes.CurrencyCode)
-
-		// TODO: translate PSP balance to formance balance object
-		_ = balance
-		accountBalances = append(accountBalances, models.PSPBalance{})
+	if err := json.Unmarshal(from.Raw, &qontoBankAccount); err != nil {
+		return models.FetchNextBalancesResponse{}, err
 	}
+
+	accountBalance := models.PSPBalance{
+		AccountReference: from.Reference,
+		Amount:           big.NewInt(qontoBankAccount.BalanceCents),
+		Asset:            *from.DefaultAsset,
+		CreatedAt:        time.Now(),
+	}
+	accountBalances := []models.PSPBalance{accountBalance}
 
 	return models.FetchNextBalancesResponse{
 		Balances: accountBalances,
