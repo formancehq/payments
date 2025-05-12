4123d99257831d3bcd070d820cb1f5e8415be67e
Crimson Thompson <crimson@formance.com>
2025-05-12 13:09:32 +0200

    Add functionality to fetch latest pool balance if at value is current time

--- internal/api/services/pools_balances_at.go
+++ internal/api/services/pools_balances_at.go
@@ -9,16 +9,34 @@ import (
 	"github.com/google/uuid"
 )
 
-func (s *Service) PoolsBalancesAt(ctx context.Context, poolID uuid.UUID, at time.Time) ([]models.AggregatedBalance, error) {
+func (s *Service) PoolsBalancesAt(
+	ctx context.Context,
+	poolID uuid.UUID,
+	at time.Time,
+) ([]models.AggregatedBalance, error) {
+	var isHistoricalQuery bool
+	now := time.Now().Truncate(time.Minute)
+	if at.Before(now.Add(-time.Minute)) {
+		isHistoricalQuery = true
+	}
+
 	pool, err := s.storage.PoolsGet(ctx, poolID)
 	if err != nil {
 		return nil, newStorageError(err, "cannot get pool")
 	}
 	res := make(map[string]*big.Int)
 	for i := range pool.PoolAccounts {
-		balances, err := s.storage.BalancesGetAt(ctx, pool.PoolAccounts[i], at)
-		if err != nil {
-			return nil, newStorageError(err, "cannot get balances")
+		var balances []*models.Balance
+		if isHistoricalQuery {
+			balances, err = s.storage.BalancesGetAt(ctx, pool.PoolAccounts[i], at)
+			if err != nil {
+				return nil, newStorageError(err, "cannot get balances")
+			}
+		} else {
+			balances, err = s.storage.BalancesGetLatest(ctx, pool.PoolAccounts[i])
+			if err != nil {
+				return nil, newStorageError(err, "cannot get balances")
+			}
 		}
 
 		for _, balance := range balances {
--- internal/api/services/pools_balances_at_test.go
+++ internal/api/services/pools_balances_at_test.go
@@ -15,7 +15,7 @@ import (
 	gomock "go.uber.org/mock/gomock"
 )
 
-func TestPoolsBalancesAt(t *testing.T) {
+func TestPoolsBalancesHistorical(t *testing.T) {
 	t.Parallel()
 
 	ctrl := gomock.NewController(t)
@@ -41,7 +41,7 @@ func TestPoolsBalancesAt(t *testing.T) {
 			Balance: big.NewInt(300),
 		},
 	}
-	at := time.Now()
+	at := time.Now().Add(-time.Hour)
 
 	tests := []struct {
 		name                  string
@@ -86,6 +86,106 @@ func TestPoolsBalancesAt(t *testing.T) {
 			}, test.poolsGetStorageErr)
 			if test.poolsGetStorageErr == nil {
 				store.EXPECT().BalancesGetAt(gomock.Any(), models.AccountID{}, at).Return(balancesResponse, test.accountsBalancesAtErr)
+			}
+
+			balances, err := s.PoolsBalancesAt(context.Background(), id, at)
+			if test.expectedError == nil {
+				require.NoError(t, err)
+				require.NotNil(t, balances)
+				foundEUR := false
+				foundUSD := false
+				for _, balance := range balances {
+					switch balance.Asset {
+					case "EUR/2":
+						require.Equal(t, big.NewInt(400), balance.Amount)
+						foundEUR = true
+					case "USD/2":
+						require.Equal(t, big.NewInt(200), balance.Amount)
+						foundUSD = true
+					default:
+						require.Fail(t, "unexpected asset")
+					}
+				}
+				require.True(t, foundEUR)
+				require.True(t, foundUSD)
+			} else {
+				require.Equal(t, test.expectedError, err)
+			}
+		})
+	}
+}
+
+func TestPoolsBalancesLatest(t *testing.T) {
+	t.Parallel()
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	store := storage.NewMockStorage(ctrl)
+	eng := engine.NewMockEngine(ctrl)
+
+	s := New(store, eng, false)
+
+	id := uuid.New()
+	poolsAccount := []models.AccountID{{}}
+	balancesResponse := []*models.Balance{
+		{
+			Asset:   "EUR/2",
+			Balance: big.NewInt(100),
+		},
+		{
+			Asset:   "USD/2",
+			Balance: big.NewInt(200),
+		},
+		{
+			Asset:   "EUR/2",
+			Balance: big.NewInt(300),
+		},
+	}
+	at := time.Now().Add(-time.Second)
+
+	tests := []struct {
+		name                  string
+		poolsGetStorageErr    error
+		accountsBalancesAtErr error
+		expectedError         error
+	}{
+		{
+			name:               "success",
+			poolsGetStorageErr: nil,
+			expectedError:      nil,
+		},
+		{
+			name:               "storage error not found",
+			poolsGetStorageErr: storage.ErrNotFound,
+			expectedError:      newStorageError(storage.ErrNotFound, "cannot get pool"),
+		},
+		{
+			name:               "other error",
+			poolsGetStorageErr: fmt.Errorf("error"),
+			expectedError:      newStorageError(fmt.Errorf("error"), "cannot get pool"),
+		},
+		{
+			name:                  "storage error not found",
+			accountsBalancesAtErr: storage.ErrNotFound,
+			expectedError:         newStorageError(storage.ErrNotFound, "cannot get balances"),
+		},
+		{
+			name:                  "other error",
+			accountsBalancesAtErr: fmt.Errorf("error"),
+			expectedError:         newStorageError(fmt.Errorf("error"), "cannot get balances"),
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			store.EXPECT().PoolsGet(gomock.Any(), id).Return(&models.Pool{
+				ID:           id,
+				Name:         "test",
+				CreatedAt:    at,
+				PoolAccounts: poolsAccount,
+			}, test.poolsGetStorageErr)
+			if test.poolsGetStorageErr == nil {
+				store.EXPECT().BalancesGetLatest(gomock.Any(), models.AccountID{}).Return(balancesResponse, test.accountsBalancesAtErr)
 			}
 
 			balances, err := s.PoolsBalancesAt(context.Background(), id, at)
