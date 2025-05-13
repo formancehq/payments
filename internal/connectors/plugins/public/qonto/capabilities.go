cc80ac33cde8b2bfda3e99a9a5e305e8c2aa6879
fabrice guery <ecolofou@gmail.com>
2025-05-13 14:30:00 +0200

    feat(CONN-92): add transfer capability

--- internal/connectors/plugins/public/qonto/capabilities.go
+++ internal/connectors/plugins/public/qonto/capabilities.go
@@ -7,4 +7,5 @@ var capabilities = []models.Capability{
 	models.CAPABILITY_FETCH_BALANCES,
 	models.CAPABILITY_FETCH_EXTERNAL_ACCOUNTS,
 	models.CAPABILITY_FETCH_PAYMENTS,
+	models.CAPABILITY_CREATE_TRANSFER,
 }
