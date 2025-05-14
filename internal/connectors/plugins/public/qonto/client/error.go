077ee13d5696ef92b070b67d4093b874dae7a61c
fabrice guery <ecolofou@gmail.com>
2025-05-14 10:24:45 +0200

    feat(CONN-92): added more coverage for config & errors; also improved Qonto's error reporting

--- internal/connectors/plugins/public/qonto/client/error.go
+++ internal/connectors/plugins/public/qonto/client/error.go
@@ -11,19 +11,30 @@ type qontoErrors struct {
 
 type qontoError struct {
 	Code   string `json:"code"`
-	Detail string `json:"Detail"`
+	Detail string `json:"detail"`
 }
 
 func (errorResponse qontoErrors) Error() error {
 	var err error
 	if len(errorResponse.Errors) == 0 {
-		err = fmt.Errorf("unexpected error, status code: %d", errorResponse.StatusCode)
+		err = fmt.Errorf("statusCode=%d, errorMessage=\"unexpected error\"", errorResponse.StatusCode)
 		return err
 	}
 
-	me := errorResponse.Errors[0]
+	if len(errorResponse.Errors) == 1 {
+		me := errorResponse.Errors[0]
+		return fmt.Errorf(
+			"statusCode=%d, errorCode=\"%s\", errorMessage=\"%s\"",
+			errorResponse.StatusCode,
+			me.Code,
+			me.Detail,
+		)
+	}
 
-	err = fmt.Errorf("%s: %s", me.Code, me.Detail)
-
+	errMsg := fmt.Sprintf("multiple errors (statusCode=%d):", errorResponse.StatusCode)
+	for _, e := range errorResponse.Errors {
+		errMsg += fmt.Sprintf(" [errorCode=\"%s\", errorMessage=\"%s\"]", e.Code, e.Detail)
+	}
+	err = fmt.Errorf(errMsg)
 	return err
 }
