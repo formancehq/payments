659807a96f0002804f45fddc49c5870d13219c65
fabrice guery <ecolofou@gmail.com>
2025-07-16 10:10:01 +0200

    fix: remove 'if not exists' to prevent silent failures

--- internal/storage/migrations/17-add-foreign-key-indices.go
+++ internal/storage/migrations/17-add-foreign-key-indices.go
@@ -13,7 +13,7 @@ func AddForeignKeyIndices(ctx context.Context, db bun.IDB) error {
 
 	// Accounts
 	_, err := db.ExecContext(ctx, `
-		CREATE INDEX CONCURRENTLY IF NOT EXISTS accounts_connector_id ON accounts (connector_id);
+		CREATE INDEX CONCURRENTLY accounts_connector_id ON accounts (connector_id);
 	`)
 	if err != nil {
 		return err
@@ -21,13 +21,13 @@ func AddForeignKeyIndices(ctx context.Context, db bun.IDB) error {
 
 	// Balances
 	_, err = db.ExecContext(ctx, `
-		CREATE INDEX CONCURRENTLY IF NOT EXISTS balances_account_id ON balances (account_id);
+		CREATE INDEX CONCURRENTLY balances_account_id ON balances (account_id);
 	`)
 	if err != nil {
 		return err
 	}
 	_, err = db.ExecContext(ctx, `
-		CREATE INDEX CONCURRENTLY IF NOT EXISTS balances_connector_id ON balances (connector_id);
+		CREATE INDEX CONCURRENTLY balances_connector_id ON balances (connector_id);
 	`)
 	if err != nil {
 		return err
@@ -35,7 +35,7 @@ func AddForeignKeyIndices(ctx context.Context, db bun.IDB) error {
 
 	// Bank_accounts
 	_, err = db.ExecContext(ctx, `
-		CREATE INDEX CONCURRENTLY IF NOT EXISTS bank_accounts_psu_id ON bank_accounts (psu_id);
+		CREATE INDEX CONCURRENTLY bank_accounts_psu_id ON bank_accounts (psu_id);
 	`)
 	if err != nil {
 		return err
@@ -43,19 +43,19 @@ func AddForeignKeyIndices(ctx context.Context, db bun.IDB) error {
 
 	// Bank_accounts_related_accounts
 	_, err = db.ExecContext(ctx, `
-		CREATE INDEX CONCURRENTLY IF NOT EXISTS bank_accounts_related_accounts_bank_account_id ON bank_accounts_related_accounts (bank_account_id);
+		CREATE INDEX CONCURRENTLY bank_accounts_related_accounts_bank_account_id ON bank_accounts_related_accounts (bank_account_id);
 	`)
 	if err != nil {
 		return err
 	}
 	_, err = db.ExecContext(ctx, `
-		CREATE INDEX CONCURRENTLY IF NOT EXISTS bank_accounts_related_accounts_account_id ON bank_accounts_related_accounts (account_id);
+		CREATE INDEX CONCURRENTLY bank_accounts_related_accounts_account_id ON bank_accounts_related_accounts (account_id);
 	`)
 	if err != nil {
 		return err
 	}
 	_, err = db.ExecContext(ctx, `
-		CREATE INDEX CONCURRENTLY IF NOT EXISTS bank_accounts_related_accounts_connector_id ON bank_accounts_related_accounts (connector_id);
+		CREATE INDEX CONCURRENTLY bank_accounts_related_accounts_connector_id ON bank_accounts_related_accounts (connector_id);
 	`)
 	if err != nil {
 		return err
@@ -63,7 +63,7 @@ func AddForeignKeyIndices(ctx context.Context, db bun.IDB) error {
 
 	// Connector_tasks_tree
 	_, err = db.ExecContext(ctx, `
-		CREATE INDEX CONCURRENTLY IF NOT EXISTS connector_tasks_tree_connector_id ON connector_tasks_tree (connector_id);
+		CREATE INDEX CONCURRENTLY connector_tasks_tree_connector_id ON connector_tasks_tree (connector_id);
 	`)
 	if err != nil {
 		return err
@@ -71,7 +71,7 @@ func AddForeignKeyIndices(ctx context.Context, db bun.IDB) error {
 
 	// Events_sent
 	_, err = db.ExecContext(ctx, `
-		CREATE INDEX CONCURRENTLY IF NOT EXISTS events_sent_connector_id ON events_sent (connector_id);
+		CREATE INDEX CONCURRENTLY events_sent_connector_id ON events_sent (connector_id);
 	`)
 	if err != nil {
 		return err
@@ -79,7 +79,7 @@ func AddForeignKeyIndices(ctx context.Context, db bun.IDB) error {
 
 	// Payment_adjustments
 	_, err = db.ExecContext(ctx, `
-		CREATE INDEX CONCURRENTLY IF NOT EXISTS payment_adjustments_payment_id ON payment_adjustments (payment_id);
+		CREATE INDEX CONCURRENTLY payment_adjustments_payment_id ON payment_adjustments (payment_id);
 	`)
 	if err != nil {
 		return err
@@ -87,7 +87,7 @@ func AddForeignKeyIndices(ctx context.Context, db bun.IDB) error {
 
 	// Payment_initiation_adjustments
 	_, err = db.ExecContext(ctx, `
-		CREATE INDEX CONCURRENTLY IF NOT EXISTS payment_initiation_adjustments_payment_initiation_id ON payment_initiation_adjustments (payment_initiation_id);
+		CREATE INDEX CONCURRENTLY payment_initiation_adjustments_payment_initiation_id ON payment_initiation_adjustments (payment_initiation_id);
 	`)
 	if err != nil {
 		return err
@@ -95,13 +95,13 @@ func AddForeignKeyIndices(ctx context.Context, db bun.IDB) error {
 
 	// Payment_initiation_related_payments
 	_, err = db.ExecContext(ctx, `
-		CREATE INDEX CONCURRENTLY IF NOT EXISTS payment_initiation_related_payments_payment_initiation_id ON payment_initiation_related_payments (payment_initiation_id);
+		CREATE INDEX CONCURRENTLY payment_initiation_related_payments_payment_initiation_id ON payment_initiation_related_payments (payment_initiation_id);
 	`)
 	if err != nil {
 		return err
 	}
 	_, err = db.ExecContext(ctx, `
-		CREATE INDEX CONCURRENTLY IF NOT EXISTS payment_initiation_related_payments_payment_id ON payment_initiation_related_payments (payment_id);
+		CREATE INDEX CONCURRENTLY payment_initiation_related_payments_payment_id ON payment_initiation_related_payments (payment_id);
 	`)
 	if err != nil {
 		return err
@@ -109,7 +109,7 @@ func AddForeignKeyIndices(ctx context.Context, db bun.IDB) error {
 
 	// Payment_initiation_reversal_adjustments
 	_, err = db.ExecContext(ctx, `
-		CREATE INDEX CONCURRENTLY IF NOT EXISTS payment_initiation_reversal_adjustments_pir_id
+		CREATE INDEX CONCURRENTLY payment_initiation_reversal_adjustments_pir_id
     ON payment_initiation_reversal_adjustments (payment_initiation_reversal_id);
 	`)
 	if err != nil {
@@ -118,13 +118,13 @@ func AddForeignKeyIndices(ctx context.Context, db bun.IDB) error {
 
 	// Payment_initiation_reversals
 	_, err = db.ExecContext(ctx, `
-		CREATE INDEX CONCURRENTLY IF NOT EXISTS payment_initiation_reversals_connector_id ON payment_initiation_reversals (connector_id);
+		CREATE INDEX CONCURRENTLY payment_initiation_reversals_connector_id ON payment_initiation_reversals (connector_id);
 	`)
 	if err != nil {
 		return err
 	}
 	_, err = db.ExecContext(ctx, `
-		CREATE INDEX CONCURRENTLY IF NOT EXISTS payment_initiation_reversals_payment_initiation_id ON payment_initiation_reversals (payment_initiation_id);
+		CREATE INDEX CONCURRENTLY payment_initiation_reversals_payment_initiation_id ON payment_initiation_reversals (payment_initiation_id);
 	`)
 	if err != nil {
 		return err
@@ -132,19 +132,19 @@ func AddForeignKeyIndices(ctx context.Context, db bun.IDB) error {
 
 	// Payment_initiations
 	_, err = db.ExecContext(ctx, `
-		CREATE INDEX CONCURRENTLY IF NOT EXISTS payment_initiations_connector_id ON payment_initiations (connector_id);
+		CREATE INDEX CONCURRENTLY payment_initiations_connector_id ON payment_initiations (connector_id);
 	`)
 	if err != nil {
 		return err
 	}
 	_, err = db.ExecContext(ctx, `
-		CREATE INDEX CONCURRENTLY IF NOT EXISTS payment_initiations_source_account_id ON payment_initiations (source_account_id);
+		CREATE INDEX CONCURRENTLY payment_initiations_source_account_id ON payment_initiations (source_account_id);
 	`)
 	if err != nil {
 		return err
 	}
 	_, err = db.ExecContext(ctx, `
-		CREATE INDEX CONCURRENTLY IF NOT EXISTS payment_initiations_destination_account_id ON payment_initiations (destination_account_id);
+		CREATE INDEX CONCURRENTLY payment_initiations_destination_account_id ON payment_initiations (destination_account_id);
 	`)
 	if err != nil {
 		return err
@@ -152,19 +152,19 @@ func AddForeignKeyIndices(ctx context.Context, db bun.IDB) error {
 
 	// Payments
 	_, err = db.ExecContext(ctx, `
-		CREATE INDEX CONCURRENTLY IF NOT EXISTS payments_connector_id ON payments (connector_id);
+		CREATE INDEX CONCURRENTLY payments_connector_id ON payments (connector_id);
 	`)
 	if err != nil {
 		return err
 	}
 	_, err = db.ExecContext(ctx, `
-		CREATE INDEX CONCURRENTLY IF NOT EXISTS payments_source_account_id ON payments (source_account_id);
+		CREATE INDEX CONCURRENTLY payments_source_account_id ON payments (source_account_id);
 	`)
 	if err != nil {
 		return err
 	}
 	_, err = db.ExecContext(ctx, `
-		CREATE INDEX CONCURRENTLY IF NOT EXISTS payments_destination_account_id ON payments (destination_account_id);
+		CREATE INDEX CONCURRENTLY payments_destination_account_id ON payments (destination_account_id);
 	`)
 	if err != nil {
 		return err
@@ -172,19 +172,19 @@ func AddForeignKeyIndices(ctx context.Context, db bun.IDB) error {
 
 	// Pool_accounts
 	_, err = db.ExecContext(ctx, `
-		CREATE INDEX CONCURRENTLY IF NOT EXISTS pool_accounts_pool_id ON pool_accounts (pool_id);
+		CREATE INDEX CONCURRENTLY pool_accounts_pool_id ON pool_accounts (pool_id);
 	`)
 	if err != nil {
 		return err
 	}
 	_, err = db.ExecContext(ctx, `
-		CREATE INDEX CONCURRENTLY IF NOT EXISTS pool_accounts_account_id ON pool_accounts (account_id);
+		CREATE INDEX CONCURRENTLY pool_accounts_account_id ON pool_accounts (account_id);
 	`)
 	if err != nil {
 		return err
 	}
 	_, err = db.ExecContext(ctx, `
-		CREATE INDEX CONCURRENTLY IF NOT EXISTS pool_accounts_connector_id ON pool_accounts (connector_id);
+		CREATE INDEX CONCURRENTLY pool_accounts_connector_id ON pool_accounts (connector_id);
 	`)
 	if err != nil {
 		return err
@@ -192,7 +192,7 @@ func AddForeignKeyIndices(ctx context.Context, db bun.IDB) error {
 
 	// Schedules
 	_, err = db.ExecContext(ctx, `
-		CREATE INDEX CONCURRENTLY IF NOT EXISTS schedules_connector_id ON schedules (connector_id);
+		CREATE INDEX CONCURRENTLY schedules_connector_id ON schedules (connector_id);
 	`)
 	if err != nil {
 		return err
@@ -200,7 +200,7 @@ func AddForeignKeyIndices(ctx context.Context, db bun.IDB) error {
 
 	// States
 	_, err = db.ExecContext(ctx, `
-		CREATE INDEX CONCURRENTLY IF NOT EXISTS states_connector_id ON states (connector_id);
+		CREATE INDEX CONCURRENTLY states_connector_id ON states (connector_id);
 	`)
 	if err != nil {
 		return err
@@ -208,7 +208,7 @@ func AddForeignKeyIndices(ctx context.Context, db bun.IDB) error {
 
 	// Webhooks
 	_, err = db.ExecContext(ctx, `
-		CREATE INDEX CONCURRENTLY IF NOT EXISTS webhooks_connector_id ON webhooks (connector_id);
+		CREATE INDEX CONCURRENTLY webhooks_connector_id ON webhooks (connector_id);
 	`)
 	if err != nil {
 		return err
@@ -216,13 +216,13 @@ func AddForeignKeyIndices(ctx context.Context, db bun.IDB) error {
 
 	// Workflows_instances
 	_, err = db.ExecContext(ctx, `
-		CREATE INDEX CONCURRENTLY IF NOT EXISTS workflows_instances_schedule_id ON workflows_instances (schedule_id);
+		CREATE INDEX CONCURRENTLY workflows_instances_schedule_id ON workflows_instances (schedule_id);
 	`)
 	if err != nil {
 		return err
 	}
 	_, err = db.ExecContext(ctx, `
-		CREATE INDEX CONCURRENTLY IF NOT EXISTS workflows_instances_connector_id ON workflows_instances (connector_id);
+		CREATE INDEX CONCURRENTLY workflows_instances_connector_id ON workflows_instances (connector_id);
 	`)
 	if err != nil {
 		return err
