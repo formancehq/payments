b3f63b83d3ed54ec3828989dacd6a35eb0859e3a
Paul Nicolas <paul.nicolas.go@gmail.com>
2025-07-18 13:58:34 +0200

    feat(models): add bank bridge models

--- internal/models/plugin.go
+++ internal/models/plugin.go
@@ -4,45 +4,34 @@ import (
 	"context"
 	"encoding/json"
 	"time"
+
+	"github.com/google/uuid"
 )
 
 const DefaultConnectorClientTimeout = 10 * time.Second
 
-type PluginConstructorFn func() Plugin
+type PluginType int
+
+const (
+	PluginTypePSP PluginType = iota
+	PluginTypeBankingBridge
+	PluginTypeBoth
+)
 
 //go:generate mockgen -source plugin.go -destination plugin_generated.go -package models . Plugin
 type Plugin interface {
 	PSPPlugin
-	PSPBankingBridge
+	BankingBridgePlugin
 
+	// Common methods
 	Name() string
 	Install(context.Context, InstallRequest) (InstallResponse, error)
 	Uninstall(context.Context, UninstallRequest) (UninstallResponse, error)
-}
-
-type PSPPlugin interface {
-	FetchNextAccounts(context.Context, FetchNextAccountsRequest) (FetchNextAccountsResponse, error)
-	FetchNextPayments(context.Context, FetchNextPaymentsRequest) (FetchNextPaymentsResponse, error)
-	FetchNextBalances(context.Context, FetchNextBalancesRequest) (FetchNextBalancesResponse, error)
-	FetchNextExternalAccounts(context.Context, FetchNextExternalAccountsRequest) (FetchNextExternalAccountsResponse, error)
-	FetchNextOthers(context.Context, FetchNextOthersRequest) (FetchNextOthersResponse, error)
-
-	CreateBankAccount(context.Context, CreateBankAccountRequest) (CreateBankAccountResponse, error)
-	CreateTransfer(context.Context, CreateTransferRequest) (CreateTransferResponse, error)
-	ReverseTransfer(context.Context, ReverseTransferRequest) (ReverseTransferResponse, error)
-	PollTransferStatus(context.Context, PollTransferStatusRequest) (PollTransferStatusResponse, error)
-	CreatePayout(context.Context, CreatePayoutRequest) (CreatePayoutResponse, error)
-	ReversePayout(context.Context, ReversePayoutRequest) (ReversePayoutResponse, error)
-	PollPayoutStatus(context.Context, PollPayoutStatusRequest) (PollPayoutStatusResponse, error)
 
 	CreateWebhooks(context.Context, CreateWebhooksRequest) (CreateWebhooksResponse, error)
+	TrimWebhook(context.Context, TrimWebhookRequest) (TrimWebhookResponse, error)
 	VerifyWebhook(context.Context, VerifyWebhookRequest) (VerifyWebhookResponse, error)
 	TranslateWebhook(context.Context, TranslateWebhookRequest) (TranslateWebhookResponse, error)
-}
-
-type PSPBankingBridge interface {
-	// This interface is intended for future banking bridge functionality.
-	// Methods will be added as the banking bridge capabilities are implemented.
 }
 
 type InstallRequest struct {
@@ -54,79 +43,11 @@ type InstallResponse struct {
 }
 
 type UninstallRequest struct {
-	ConnectorID string
+	ConnectorID    string
+	WebhookConfigs []PSPWebhookConfig
 }
 
 type UninstallResponse struct{}
-
-type FetchNextAccountsRequest struct {
-	FromPayload json.RawMessage
-	State       json.RawMessage
-	PageSize    int
-}
-
-type FetchNextAccountsResponse struct {
-	Accounts []PSPAccount
-	NewState json.RawMessage
-	HasMore  bool
-}
-
-type FetchNextExternalAccountsRequest struct {
-	FromPayload json.RawMessage
-	State       json.RawMessage
-	PageSize    int
-}
-
-type FetchNextExternalAccountsResponse struct {
-	ExternalAccounts []PSPAccount
-	NewState         json.RawMessage
-	HasMore          bool
-}
-
-type FetchNextPaymentsRequest struct {
-	FromPayload json.RawMessage
-	State       json.RawMessage
-	PageSize    int
-}
-
-type FetchNextPaymentsResponse struct {
-	Payments []PSPPayment
-	NewState json.RawMessage
-	HasMore  bool
-}
-
-type FetchNextOthersRequest struct {
-	Name        string
-	FromPayload json.RawMessage
-	State       json.RawMessage
-	PageSize    int
-}
-
-type FetchNextOthersResponse struct {
-	Others   []PSPOther
-	NewState json.RawMessage
-	HasMore  bool
-}
-
-type FetchNextBalancesRequest struct {
-	FromPayload json.RawMessage
-	State       json.RawMessage
-	PageSize    int
-}
-
-type FetchNextBalancesResponse struct {
-	Balances []PSPBalance
-	NewState json.RawMessage
-	HasMore  bool
-}
-
-type CreateBankAccountRequest struct {
-	BankAccount BankAccount
-}
-
-type CreateBankAccountResponse struct {
-	RelatedAccount PSPAccount
-}
 
 type CreateWebhooksRequest struct {
 	FromPayload    json.RawMessage
@@ -139,20 +60,13 @@ type CreateWebhooksResponse struct {
 	Others  []PSPOther // used by plugin workflow
 }
 
-type TranslateWebhookRequest struct {
-	Name    string
+type TrimWebhookRequest struct {
 	Webhook PSPWebhook
 	Config  *WebhookConfig
 }
 
-type WebhookResponse struct {
-	Account         *PSPAccount
-	ExternalAccount *PSPAccount
-	Payment         *PSPPayment
-}
-
-type TranslateWebhookResponse struct {
-	Responses []WebhookResponse
+type TrimWebhookResponse struct {
+	Webhook PSPWebhook
 }
 
 type VerifyWebhookRequest struct {
@@ -164,72 +78,111 @@ type VerifyWebhookResponse struct {
 	WebhookIdempotencyKey *string
 }
 
-type CreateTransferRequest struct {
-	PaymentInitiation PSPPaymentInitiation
+type TranslateWebhookRequest struct {
+	Name    string
+	Webhook PSPWebhook
+	Config  *WebhookConfig
 }
 
-type CreateTransferResponse struct {
-	// If payment is immediately available, it will be return here and
-	// the workflow will be terminated
-	Payment *PSPPayment
-	// Otherwise, the payment will be nil and the transfer ID will be returned
-	// to be polled regularly until the payment is available
-	PollingTransferID *string
+type TranslateWebhookResponse struct {
+	Responses []WebhookResponse
 }
 
-type ReverseTransferRequest struct {
-	PaymentInitiationReversal PSPPaymentInitiationReversal
-}
-type ReverseTransferResponse struct {
-	Payment PSPPayment
-}
+type WebhookResponse struct {
+	Account         *PSPAccount
+	ExternalAccount *PSPAccount
+	Payment         *PSPPayment
 
-type PollTransferStatusRequest struct {
-	TransferID string
-}
-
-type PollTransferStatusResponse struct {
-	// If nil, the payment is not yet available and the function will be called
-	// again later
-	// If not, the payment is available and the workflow will be terminated
-	Payment *PSPPayment
-
-	// If not nil, it means that the transfer failed, the payment initiation
-	// will be marked as fail and the workflow will be terminated
-	Error *string
+	// Webhooks related to banking bridges
+	UserLinkSessionFinished         *PSPUserLinkSessionFinished
+	DataReadyToFetch                *PSPDataReadyToFetch
+	UserConnectionPendingDisconnect *PSPUserConnectionPendingDisconnect
+	UserConnectionDisconnected      *PSPUserConnectionDisconnected
 }
 
-type CreatePayoutRequest struct {
-	PaymentInitiation PSPPaymentInitiation
+type PSPDataReadyToFetch struct {
+	ID          *string
+	FromPayload json.RawMessage
 }
 
-type CreatePayoutResponse struct {
-	// If payment is immediately available, it will be return here and
-	// the workflow will be terminated
-	Payment *PSPPayment
-	// Otherwise, the payment will be nil and the payout ID will be returned
-	// to be polled regularly until the payment is available
-	PollingPayoutID *string
+type PSPDataToDelete struct {
+	ConnectionID string
+
+	// If filled, the account and all its data will be deleted. The connection
+	// ID will still be available in the database.
+	// If not filled, all data associated with the connection will be deleted.
+	AccountID *string
+
+	FromPayload json.RawMessage
 }
 
-type ReversePayoutRequest struct {
-	PaymentInitiationReversal PSPPaymentInitiationReversal
-}
-type ReversePayoutResponse struct {
-	Payment PSPPayment
-}
-
-type PollPayoutStatusRequest struct {
-	PayoutID string
+type PSPUserConnectionPendingDisconnect struct {
+	ConnectionID string
+	At           time.Time
+	Reason       *string
 }
 
-type PollPayoutStatusResponse struct {
-	// If nil, the payment is not yet available and the function will be called
-	// again later
-	// If not, the payment is available and the workflow will be terminated
-	Payment *PSPPayment
+type UserConnectionPendingDisconnect struct {
+	PsuID        uuid.UUID
+	ConnectorID  ConnectorID
+	ConnectionID string
+	At           time.Time
+	Reason       *string
+}
 
-	// If not nil, it means that the payout failed, the payment initiation
-	// will be marked as fail and the workflow will be terminated
-	Error *string
+func (u UserConnectionPendingDisconnect) IdempotencyKey() string {
+	return IdempotencyKey(u)
+}
+
+type PSPUserConnectionDisconnected struct {
+	ConnectionID string
+	At           time.Time
+	Reason       *string
+}
+
+type UserConnectionDisconnected struct {
+	PsuID        uuid.UUID
+	ConnectorID  ConnectorID
+	ConnectionID string
+	At           time.Time
+	Reason       *string
+}
+
+func (u UserConnectionDisconnected) IdempotencyKey() string {
+	return IdempotencyKey(u)
+}
+
+type PSPUserLinkSessionFinished struct {
+	AttemptID uuid.UUID
+	Status    PSUBankBridgeConnectionAttemptStatus
+	Error     *string
+}
+
+type UserLinkSessionFinished struct {
+	PsuID       uuid.UUID
+	ConnectorID ConnectorID
+	AttemptID   uuid.UUID
+	Status      PSUBankBridgeConnectionAttemptStatus
+	Error       *string
+}
+
+func (u UserLinkSessionFinished) IdempotencyKey() string {
+	return IdempotencyKey(u)
+}
+
+type UserConnectionDataSynced struct {
+	PsuID        uuid.UUID
+	ConnectorID  ConnectorID
+	ConnectionID string
+	At           time.Time
+}
+
+func (u UserConnectionDataSynced) IdempotencyKey() string {
+	return IdempotencyKey(u)
+}
+
+type BankBridgeFromPayload struct {
+	PSUBankBridge           *PSUBankBridge
+	PSUBankBridgeConnection *PSUBankBridgeConnection
+	FromPayload             json.RawMessage
 }
