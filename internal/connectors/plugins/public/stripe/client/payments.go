45b33bdc145ef3d1b5019475f013b87e934018dd
Crimson Thompson <crimson@formance.com>
2025-08-01 17:56:36 +0200

    Reverse order of items returned by Stripe List APIs

--- internal/connectors/plugins/public/stripe/client/payments.go
+++ internal/connectors/plugins/public/stripe/client/payments.go
@@ -65,12 +65,14 @@ func (c *client) GetPayments(
 	expandBalanceTransactionParams(params)
 
 	itr := c.balanceTransactionClient.List(params)
-	results = append(results, itr.BalanceTransactionList().Data...)
+	data := reverseTransactions(itr.BalanceTransactionList().Data)
+	results = append(results, data...)
 	if len(results) == 0 {
 		return results, timeline, itr.BalanceTransactionList().ListMeta.HasMore, wrapSDKErr(itr.Err())
 	}
 
 	timeline.LatestID = results[len(results)-1].ID
+	c.logger.WithField("account", accountID).WithField("latest_id", timeline.LatestID).Debugf("set latest id after batch with %d entries", len(results))
 	return results, timeline, itr.BalanceTransactionList().ListMeta.HasMore, wrapSDKErr(itr.Err())
 }
 
@@ -83,4 +85,14 @@ func expandBalanceTransactionParams(params *stripe.BalanceTransactionListParams)
 	params.AddExpand(expandSourceTransfer)
 	params.AddExpand(expandSourcePaymentIntent)
 	params.AddExpand(expandSourceRefundPaymentIntent)
+}
+
+// Stripe now returns data in reverse chronological order no matter which params we provide so we need to reverse the slice
+func reverseTransactions(in []*stripe.BalanceTransaction) []*stripe.BalanceTransaction {
+	out := make([]*stripe.BalanceTransaction, len(in))
+	copy(out, in)
+	for i, j := 0, len(out)-1; i < j; i, j = i+1, j-1 {
+		out[i], out[j] = out[j], out[i]
+	}
+	return out
 }
