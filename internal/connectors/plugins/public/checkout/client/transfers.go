70ce6c6652909e7cd4e1acf2ce6324843ebd8ac8
Nicolas Torion <nicolas.torion@bearstudio.fr>
2025-08-25 16:42:52 +0200

    Added transfers initiate

---
+++ internal/connectors/plugins/public/checkout/client/transfers.go
@@ -0,0 +1,80 @@
+package client
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/checkout/checkout-sdk-go/common"
+	"github.com/checkout/checkout-sdk-go/transfers"
+
+	"github.com/formancehq/payments/internal/connectors/metrics"
+)
+
+type TransferRequest struct {
+	Reference string `json:"reference,omitempty"`
+	Reason    string `json:"reason,omitempty"`
+
+	Source struct {
+		EntityID string `json:"entity_id"`
+		Currency string `json:"currency"`
+	} `json:"source"`
+
+	Destination struct {
+		EntityID string `json:"entity_id"`
+		Currency string `json:"currency"`
+	} `json:"destination"`
+
+	Amount int64  `json:"amount"`
+	IdempotencyKey string `json:"-"`
+}
+
+type TransferResponse struct {
+	ID        string          `json:"id"`
+	Status    string          `json:"status,omitempty"`
+	CreatedOn *time.Time      `json:"created_on,omitempty"`
+	Raw       any             `json:"raw,omitempty"`
+}
+
+type sdkTransferRequest = transfers.TransferRequest
+
+func (c *client) InitiateTransfer(ctx context.Context, tr *TransferRequest) (*TransferResponse, error) {
+	ctx = context.WithValue(ctx, metrics.MetricOperationContextKey, "initiate_transfer")
+
+	if tr == nil {
+		return nil, fmt.Errorf("missing transfer request")
+	}
+	if tr.Amount <= 0 {
+		return nil, fmt.Errorf("amount must be > 0")
+	}
+
+	req := transfers.TransferRequest{
+		Reference: tr.Reference,
+		TransferType: "commission",
+		Source: &transfers.TransferSourceRequest{
+			Id: tr.Source.EntityID,
+			Amount: tr.Amount,
+			Currency: common.Currency(tr.Source.Currency),
+		},
+		Destination: &transfers.TransferDestinationRequest{
+			Id: tr.Destination.EntityID,
+		},
+	}
+
+	var idem *string
+	if tr.IdempotencyKey != "" {
+		idem = &tr.IdempotencyKey
+	}
+
+	resp, err := c.sdk.Transfers.InitiateTransferOfFounds(req, idem)
+	if err != nil {
+		return nil, fmt.Errorf("checkout.accounts.transfers: %w", err)
+	}
+
+	out := &TransferResponse{
+		ID:        resp.Id,
+		Status:    resp.Status,
+		Raw:       resp,
+	}
+	return out, nil
+}
--- internal/connectors/plugins/public/checkout/transfers.go
+++ internal/connectors/plugins/public/checkout/transfers.go
@@ -2,9 +2,16 @@ package checkout
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"strings"
+	"time"
+	"math/big"
 
 	"github.com/formancehq/payments/internal/connectors/plugins/public/checkout/client"
 	"github.com/formancehq/payments/internal/models"
+	
+	"github.com/formancehq/go-libs/v3/currency"
 )
 
 func (p *Plugin) createTransfer(ctx context.Context, pi models.PSPPaymentInitiation) (*models.PSPPayment, error) {
@@ -12,33 +19,76 @@ func (p *Plugin) createTransfer(ctx context.Context, pi models.PSPPaymentInitiat
 		return nil, err
 	}
 
-	// TODO: Since we are in minor units currency in the PSPPaymentInitiation
-	// object, we sometimes need to put back the amount in float for
-	// the PSP. You can use the next methods to do that.
-	// curr, precision, err := currency.GetCurrencyAndPrecisionFromAsset(supportedCurrenciesWithDecimal, pi.Asset)
-	// if err != nil {
-	//	 return nil, fmt.Errorf("failed to get currency and precision from asset: %v: %w", err, models.ErrInvalidRequest)
-	// }
+	curr, _, err := currency.GetCurrencyAndPrecisionFromAsset(supportedCurrenciesWithDecimal, pi.Asset)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get currency from asset %q: %w", pi.Asset, models.ErrInvalidRequest)
+	}
 
-	// amount, err := currency.GetStringAmountFromBigIntWithPrecision(pi.Amount, precision)
-	// if err != nil {
-	// 	 return nil, fmt.Errorf("failed to get string amount from big int: %v: %w", err, models.ErrInvalidRequest)
-	// }
+	if !pi.Amount.IsInt64() {
+		return nil, fmt.Errorf("amount overflows int64: %w", models.ErrInvalidRequest)
+	}
+	amountMinor := pi.Amount.Int64()
 
-	resp, err := p.client.InitiateTransfer(
-		ctx,
-		&client.TransferRequest{
-			// TODO: fill transfer request
-		},
-	)
+	var srcEnt, dstEnt string
+	if pi.SourceAccount != nil {
+		srcEnt = pi.SourceAccount.Reference
+	}
+	if pi.DestinationAccount != nil {
+		dstEnt = pi.DestinationAccount.Reference
+	}
+
+	tr := &client.TransferRequest{
+		Reference: pi.Reference,
+		Reason:    "Formance transfer",
+	}
+	tr.Source.EntityID = srcEnt
+	tr.Source.Currency = curr
+	tr.Destination.EntityID = dstEnt
+	tr.Destination.Currency = curr
+	tr.Amount = amountMinor
+
+	resp, err := p.client.InitiateTransfer(ctx, tr)
 	if err != nil {
 		return nil, err
 	}
 
-	return transferToPayment(resp)
+	raw, _ := json.Marshal(resp)
+
+	createdAt := time.Now().UTC()
+	if resp.CreatedOn != nil {
+		createdAt = *resp.CreatedOn
+	}
+
+	asset := currency.FormatAsset(supportedCurrenciesWithDecimal, tr.Source.Currency)
+
+	return &models.PSPPayment{
+		ParentReference: "",
+		Reference:       resp.ID,
+		CreatedAt:       createdAt,
+		Type:            models.PAYMENT_TYPE_TRANSFER,
+		Status:          mapTransferStatusToPaymentStatus(resp.Status),
+		Amount:          big.NewInt(tr.Amount),
+		Asset:           asset,
+		Raw:      		 raw,
+	}, nil
 }
 
-func transferToPayment(transfer *client.TransferResponse) (*models.PSPPayment, error) {
-	// TODO: translate transfer to formance payment object
-	return nil, nil
+func mapTransferStatusToPaymentStatus(s string) models.PaymentStatus {
+	ls := strings.ToLower(strings.TrimSpace(s))
+	switch ls {
+	case "pending", "requested", "processing":
+		return models.PAYMENT_STATUS_PENDING
+	case "approved", "succeeded", "completed", "successful":
+		return models.PAYMENT_STATUS_SUCCEEDED
+	case "failed", "declined", "rejected", "error":
+		return models.PAYMENT_STATUS_FAILED
+	case "canceled", "cancelled", "voided":
+		return models.PAYMENT_STATUS_CANCELLED
+	case "reversed":
+		return models.PAYMENT_STATUS_REFUND_REVERSED
+	default:
+		return models.PAYMENT_STATUS_OTHER
+	}
 }
+
+func strPtr(s string) *string { return &s }
