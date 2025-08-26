a936488fd0254dc355b9e1cb6b04d2f82e0f9c5e
Nicolas Torion <nicolas.torion@bearstudio.fr>
2025-08-26 14:04:14 +0200

    Clean comments

--- internal/connectors/plugins/public/checkout/capabilities.go
+++ internal/connectors/plugins/public/checkout/capabilities.go
@@ -3,11 +3,9 @@ package checkout
 import "github.com/formancehq/payments/internal/models"
 
 var capabilities = []models.Capability{
-	// TODO: add or remove capabilities depending on what data your connector
-	// intends to import
-	models.CAPABILITY_FETCH_ACCOUNTS, // OK
-	models.CAPABILITY_FETCH_BALANCES, // OK
-	models.CAPABILITY_FETCH_PAYMENTS, // OK
+	models.CAPABILITY_FETCH_ACCOUNTS,
+	models.CAPABILITY_FETCH_BALANCES,
+	models.CAPABILITY_FETCH_PAYMENTS,
 
 	models.CAPABILITY_CREATE_TRANSFER,
 	models.CAPABILITY_CREATE_PAYOUT,
