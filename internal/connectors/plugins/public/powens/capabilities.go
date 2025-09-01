6259885e011a735bd2b4f15f7baab2b324db1eee
Paul Nicolas <paul.nicolas.go@gmail.com>
2025-09-01 11:12:40 +0200

    fix(bankbridge): fix missing capabilities

--- internal/connectors/plugins/public/powens/capabilities.go
+++ internal/connectors/plugins/public/powens/capabilities.go
@@ -2,4 +2,10 @@ package powens
 
 import "github.com/formancehq/payments/internal/models"
 
-var capabilities = []models.Capability{}
+var capabilities = []models.Capability{
+	models.CAPABILITY_FETCH_ACCOUNTS,
+	models.CAPABILITY_FETCH_EXTERNAL_ACCOUNTS,
+	models.CAPABILITY_FETCH_PAYMENTS,
+	models.CAPABILITY_CREATE_WEBHOOKS,
+	models.CAPABILITY_TRANSLATE_WEBHOOKS,
+}
