512311cab9d71057d4248cd311266508019c9c47
Clément Salaün <salaun.clement@gmail.com>
2025-09-03 18:30:16 +0200

    wip: scaffolding
    Signed-off-by: Clément Salaün <salaun.clement@gmail.com>

---
+++ internal/connectors/plugins/public/coinbaseprime/client/accounts.go
@@ -0,0 +1,16 @@
+package client
+
+import (
+	"context"
+    
+	"github.com/formancehq/payments/internal/connectors/metrics"
+)
+
+type Account struct {}
+
+func (c *client) GetAccounts(ctx context.Context, page int, pageSize int) ([]*Account, error) {
+	ctx = context.WithValue(ctx, metrics.MetricOperationContextKey, "list_accounts")
+
+	// TODO: call PSP to get accounts
+    return nil, nil
+}
---
+++ internal/connectors/plugins/public/coinbaseprime/client/client.go
@@ -0,0 +1,54 @@
+package client
+
+import (
+	"context"
+
+	"github.com/formancehq/payments/internal/connectors/httpwrapper"
+	"github.com/formancehq/payments/internal/connectors/metrics"
+)
+
+//go:generate mockgen -source client.go -destination client_generated.go -package client . Client
+type Client interface {
+	GetAccounts(ctx context.Context, page int, pageSize int) ([]*Account, error)
+	GetAccountBalances(ctx context.Context) ([]*Balance, error)
+	GetExternalAccounts(ctx context.Context, page int, pageSize int) ([]*ExternalAccount, error)
+	GetTransactions(ctx context.Context, page, pageSize int) ([]*Transaction, error)
+	InitiateTransfer(ctx context.Context, tr *TransferRequest) (*TransferResponse, error)
+	InitiatePayout(ctx context.Context, pr *PayoutRequest) (*PayoutResponse, error)
+}
+
+type Config struct {
+	Credentials string
+}
+
+type client struct {
+	httpClient httpwrapper.Client
+
+	// TODO: fill config parameters
+	// You may need fields here for authentication purpose
+	credentials string
+}
+
+func New(cfg Config) *client {
+	config := &httpwrapper.Config{
+		// TODO: you can set an underlying http transport in metrics.TransportOpts for authentication for example
+		Transport: metrics.NewTransport("coinbaseprime", metrics.TransportOpts{}),
+		// TODO: if the PSP requires special http status code handling, you can override the default handling by setting a
+		// custom HttpErrorCheckerFn like below
+		// HttpErrorCheckerFn: func(statusCode int) error {
+		// 	if statusCode == http.StatusNotFound {
+		// 		return nil
+		// 	} else if statusCode >= http.StatusBadRequest && statusCode < http.StatusInternalServerError {
+		// 		return httpwrapper.ErrStatusCodeClientError
+		// 	} else if statusCode >= http.StatusInternalServerError {
+		// 		return httpwrapper.ErrStatusCodeServerError
+		// 	}
+		// 	return nil
+		// },
+	}
+
+	return &client{
+		httpClient:  httpwrapper.NewClient(config),
+		credentials: cfg.Credentials,
+	}
+}
