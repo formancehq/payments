88317abc09b89791a38b359404e94fef693357fe
Clément Salaün <salaun.clement@gmail.com>
2025-09-03 18:30:16 +0200

    wip: scaffolding
    Signed-off-by: Clément Salaün <salaun.clement@gmail.com>

---
+++ internal/connectors/plugins/public/coinbaseprime/workflow.go
@@ -0,0 +1,33 @@
+package coinbaseprime
+
+import "github.com/formancehq/payments/internal/models"
+
+func workflow() models.ConnectorTasksTree {
+	return []models.ConnectorTaskTree{
+		{
+
+			TaskType:     models.TASK_FETCH_ACCOUNTS,
+			Name:         "fetch_accounts",
+			Periodically: true,
+			NextTasks: []models.ConnectorTaskTree{
+				{
+					TaskType:     models.TASK_FETCH_BALANCES,
+					Name:         "fetch_balances",
+					Periodically: true,
+					NextTasks:    []models.ConnectorTaskTree{},
+				},
+			},
+		},
+		// {
+		// 	TaskType:  models.TASK_FETCH_PAYMENTS,
+		// 	Name:      "fetch_payments",
+		// 	NextTasks: []models.ConnectorTaskTree{},
+		// },
+		// {
+		// 	TaskType:     models.TASK_FETCH_EXTERNAL_ACCOUNTS,
+		// 	Name:         "fetch_external_accounts",
+		// 	Periodically: true,
+		// 	NextTasks:    []models.ConnectorTaskTree{},
+		// },
+	}
+}
