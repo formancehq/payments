b70eae7af14b61d996d09bde8261eb4817213cae
fabrice guery <ecolofou@gmail.com>
2025-11-18 23:12:13 +0100

    fix: wrong pointer use in loop

--- internal/storage/balances.go
+++ internal/storage/balances.go
@@ -85,13 +85,14 @@ func (s *store) BalancesUpsert(ctx context.Context, balances []models.Balance) e
 				return e("failed to marshal balance event payload", err)
 			}
 
+			connectorID := balance.AccountID.ConnectorID
 			outboxEvent := models.OutboxEvent{
 				EventType:      events.EventTypeSavedBalances,
 				EntityID:       balance.AccountID.String(),
 				Payload:        payloadBytes,
 				CreatedAt:      time.Now().UTC(),
 				Status:         models.OUTBOX_STATUS_PENDING,
-				ConnectorID:    &balance.AccountID.ConnectorID,
+				ConnectorID:    &connectorID,
 				IdempotencyKey: balance.IdempotencyKey(),
 			}
 
