e80ba1912a24198edd94dc2d52ca0fb312c1c313
fabrice guery <ecolofou@gmail.com>
2025-11-20 08:33:23 +0100

    make e2e test less brittle, save eventID as idempotency key in publication

--- test/e2e/api_pools_test.go
+++ test/e2e/api_pools_test.go
@@ -34,12 +34,12 @@ var _ = Context("Payments API Pools", Serial, func() {
 
 	app = NewTestServer(func() Configuration {
 		return Configuration{
-			Stack:                     stack,
-			PostgresConfiguration:     db.GetValue().ConnectionOptions(),
-			NatsURL:                   natsServer.GetValue().ClientURL(),
-			TemporalNamespace:         temporalServer.GetValue().DefaultNamespace(),
-			TemporalAddress:           temporalServer.GetValue().Address(),
-			Output:                    GinkgoWriter,
+			Stack:                      stack,
+			PostgresConfiguration:      db.GetValue().ConnectionOptions(),
+			NatsURL:                    natsServer.GetValue().ClientURL(),
+			TemporalNamespace:          temporalServer.GetValue().DefaultNamespace(),
+			TemporalAddress:            temporalServer.GetValue().Address(),
+			Output:                     GinkgoWriter,
 			SkipOutboxScheduleCreation: true,
 		}
 	})
@@ -333,12 +333,19 @@ var _ = Context("Payments API Pools", Serial, func() {
 			id := uuid.New()
 			connectorConf := newV3ConnectorConfigFn()(id)
 
-			_, err = GeneratePSPData(connectorConf.Directory, 1)
-			Expect(err).To(BeNil())
-
 			connectorID, err = installV3Connector(ctx, app.GetValue(), connectorConf, uuid.New())
 			Expect(err).To(BeNil())
 
+			_, err = GeneratePSPData(connectorConf.Directory, 1)
+			Expect(err).To(BeNil())
+
+			// Wait for at least one account to be created to ensure the connector workflow has started
+			Eventually(func() int {
+				n, _ := CountOutboxEventsByType(ctx, app.GetValue(), events.EventTypeSavedAccounts)
+				return n
+			}).WithTimeout(10 * time.Second).WithPolling(100 * time.Millisecond).Should(Equal(1))
+
+			// Wait for balance event to appear in the outbox
 			var msg internalEvents.BalanceMessagePayload
 			Eventually(func() bool {
 				payloads, err := LoadOutboxPayloadsByType(ctx, app.GetValue(), events.EventTypeSavedBalances)
@@ -353,7 +360,7 @@ var _ = Context("Payments API Pools", Serial, func() {
 					}
 				}
 				return false
-			}).WithTimeout(10 * time.Second).WithPolling(1 * time.Second).Should(BeTrue())
+			}).WithTimeout(10 * time.Second).WithPolling(100 * time.Millisecond).Should(BeTrue())
 
 			balanceResponse, err := app.GetValue().SDK().Payments.V1.GetAccountBalances(ctx, operations.GetAccountBalancesRequest{
 				AccountID: msg.AccountID,
