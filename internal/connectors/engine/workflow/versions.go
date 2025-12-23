fb8b4df4e321d71514538f60507d17d199725f10
fabrice guery <ecolofou@gmail.com>
2025-12-23 11:32:32 +0100

    fix: simplify a bit the versionning etc

---
+++ internal/connectors/engine/workflow/versions.go
@@ -0,0 +1,24 @@
+package workflow
+
+import "go.temporal.io/sdk/workflow"
+
+const (
+	versionFlagOutboxPatternEnabled  = "event_outbox_pattern_enabled"
+	versionFlagRunNextTaskAsActivity = "run_next_task_as_activity"
+)
+
+func IsEventOutboxPatternEnabled(ctx workflow.Context) bool {
+	version := workflow.GetVersion(ctx, versionFlagOutboxPatternEnabled, workflow.DefaultVersion, 1)
+	if version > workflow.DefaultVersion {
+		return true
+	}
+	return false
+}
+
+func IsRunNextTaskAsActivityEnabled(ctx workflow.Context) bool {
+	version := workflow.GetVersion(ctx, versionFlagRunNextTaskAsActivity, workflow.DefaultVersion, 1)
+	if version == workflow.DefaultVersion {
+		return true
+	}
+	return false
+}
