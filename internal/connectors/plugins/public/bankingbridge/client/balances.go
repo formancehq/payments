0fbf56a3a13efaa66372111aa4dc07ef2ab938cf
Crimson Thompson <crimson@formance.com>
2026-03-02 14:57:00 +0100

    Update client implementation

--- internal/connectors/plugins/public/bankingbridge/client/balances.go
+++ internal/connectors/plugins/public/bankingbridge/client/balances.go
@@ -2,22 +2,54 @@ package client
 
 import (
 	"context"
+	"fmt"
+	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/formancehq/payments/internal/connectors/metrics"
+	"github.com/formancehq/payments/internal/utils/errors"
 )
 
 type Balance struct {
-	AccountReference string `json:"accountReference"`
-	Asset            string `json:"asset"`
-	AmountInMinors   int64  `json:"amountInMinors"`
-	ReportedAt       string `json:"reportedAt"`
-	ImportedAt       string `json:"importedAt"`
-	UpdatedAt        string `json:"updatedAt"`
+	AccountReference string    `json:"accountReference"`
+	Asset            string    `json:"asset"`
+	AmountInMinors   int64     `json:"amountInMinors"`
+	ReportedAt       time.Time `json:"reportedAt"`
+
+	ImportedAt time.Time `json:"importedAt"`
+	UpdatedAt  time.Time `json:"updatedAt"`
 }
 
 func (c *client) GetAccountBalances(ctx context.Context, cursor string, pageSize int) ([]Balance, bool, string, error) {
 	ctx = context.WithValue(ctx, metrics.MetricOperationContextKey, "list_account_balances")
 
-	// TODO: call PSP to have balances
-	return nil, false, "", nil
+	endpoint := fmt.Sprintf("%s/v1/connectors/balances", c.endpoint)
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, endpoint, http.NoBody)
+	if err != nil {
+		return nil, false, "", fmt.Errorf("failed to create balances request: %w", err)
+	}
+
+	q := req.URL.Query()
+	q.Add("pageSize", strconv.Itoa(pageSize))
+	q.Add("cursor", cursor)
+	req.URL.RawQuery = q.Encode()
+
+	var body struct {
+		Cursor struct {
+			PageSize int64     `json:"pageSize"`
+			Next     string    `json:"next"`
+			Previous string    `json:"previous"`
+			HasMore  bool      `json:"hasMore"`
+			Data     []Balance `json:"data"`
+		} `json:"cursor"`
+	}
+	statusCode, err := c.httpClient.Do(ctx, req, &body, nil)
+	if err != nil {
+		return nil, false, "", errors.NewWrappedError(
+			fmt.Errorf("failed to get balances, status code: %d", statusCode),
+			err,
+		)
+	}
+	return body.Cursor.Data, body.Cursor.HasMore, body.Cursor.Next, nil
 }
--- internal/connectors/plugins/public/bankingbridge/client/transactions.go
+++ internal/connectors/plugins/public/bankingbridge/client/transactions.go
@@ -2,33 +2,72 @@ package client
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/formancehq/payments/internal/connectors/metrics"
+	"github.com/formancehq/payments/internal/utils/errors"
 )
 
 type Transaction struct {
-	ID                   string `json:"id"`
-	AccountReference     string `json:"accountReference"`
-	BookedAt             string `json:"bookedAt"`
-	SettledAt            string `json:"settledAt"`
-	Status               string `json:"status"`
-	Asset                string `json:"asset"`
-	AmountInMinors       int64  `json:"amountInMinors"`
-	Scheme               string `json:"scheme"`
-	IsReversal           bool   `json:"isReversal"`
-	IsBatch              bool   `json:"isBatch"`
-	NumberOfTransactions int    `json:"numberOfTransactions"`
-	EntryReference       string `json:"entryReference"`
-	ServicerReference    string `json:"servicerReference"`
-	BatchMessageId       string `json:"batchMessageId"`
-	BatchPaymentInfoId   string `json:"batchPaymentInfoId"`
-	ImportedAt           string `json:"importedAt"`
-	UpdatedAt            string `json:"updatedAt"`
+	ID                   string    `json:"id"`
+	AccountReference     string    `json:"accountReference"`
+	BookedAt             time.Time `json:"bookedAt"`
+	BookingDate          string    `json:"bookingDate"`
+	ValutaDate           string    `json:"valutaDate"`
+	Status               string    `json:"status"`
+	Asset                string    `json:"asset"`
+	AmountInMinors       int64     `json:"amountInMinors"`
+	BankTransactionCode  string    `json:"bankTransactionCode"`
+	IsReversal           bool      `json:"isReversal"`
+	IsBatch              bool      `json:"isBatch"`
+	NumberOfTransactions uint64    `json:"numberOfTransactions"`
+
+	EntryReference     string `json:"entryReference,omitempty"`
+	ServicerReference  string `json:"servicerReference,omitempty"`
+	BatchMessageID     string `json:"batchMessageId,omitempty"`
+	BatchPaymentInfoID string `json:"batchPaymentInfoId,omitempty"`
+
+	// There might be fewer details than there are transactions in bulk transactions
+	// the bank does not always provide a detailed breakdown of all the underlying transactions
+	Details []json.RawMessage `json:"details"`
+
+	ImportedAt time.Time `json:"importedAt"`
+	UpdatedAt  time.Time `json:"updatedAt"`
 }
 
 func (c *client) GetTransactions(ctx context.Context, cursor string, pageSize int) ([]Transaction, bool, string, error) {
 	ctx = context.WithValue(ctx, metrics.MetricOperationContextKey, "list_transactions")
 
-	// TODO: call PSP to get transactions
-	return nil, false, "", nil
+	endpoint := fmt.Sprintf("%s/v1/connectors/transactions", c.endpoint)
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, endpoint, http.NoBody)
+	if err != nil {
+		return nil, false, "", fmt.Errorf("failed to create transactions request: %w", err)
+	}
+
+	q := req.URL.Query()
+	q.Add("pageSize", strconv.Itoa(pageSize))
+	q.Add("cursor", cursor)
+	req.URL.RawQuery = q.Encode()
+
+	var body struct {
+		Cursor struct {
+			PageSize int64         `json:"pageSize"`
+			Next     string        `json:"next"`
+			Previous string        `json:"previous"`
+			HasMore  bool          `json:"hasMore"`
+			Data     []Transaction `json:"data"`
+		} `json:"cursor"`
+	}
+	statusCode, err := c.httpClient.Do(ctx, req, &body, nil)
+	if err != nil {
+		return nil, false, "", errors.NewWrappedError(
+			fmt.Errorf("failed to get transactions, status code: %d", statusCode),
+			err,
+		)
+	}
+	return body.Cursor.Data, body.Cursor.HasMore, body.Cursor.Next, nil
 }
